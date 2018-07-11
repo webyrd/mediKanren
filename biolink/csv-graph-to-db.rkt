bfc38a9e93e8611b29f1733a3303bbf4a5620452
gregr <greg.weiqi@gmail.com>
2018-07-10 22:04:45 -0400

    Process edge-related data

--- biolink/csv-graph-to-db.rkt
+++ biolink/csv-graph-to-db.rkt
@@ -25,9 +25,9 @@
 (define fnout-categories           "categories.scm")
 (define fnout-concepts-by-category "concepts-by-category.scm")
 (define fnout-edges                "edges.scm")
-;; TODO: these need block offsetting.
-(define fnout-edges-by-subject      "edges-by-subject.detail")
-(define fnout-edges-by-object       "edges-by-object.detail")
+(define fnout-predicates           "predicates.scm")
+(define fnout-edges-by-subject     "edges-by-subject.detail")
+(define fnout-edges-by-object      "edges-by-object.detail")
 (define (fname-offset fname) (string-append fname ".offset"))
 
 (define nodeprop-header-expected ":ID,propname,value")
@@ -108,6 +108,108 @@
               out-concepts-by-category out-offset-concepts-by-category vcids)))
       (_ (error "malformed row:" (nodeprops 'current))))))
 
+(define (process-edges in-edge in-edgeprop
+                       out-predicates
+                       out-edges out-offset-edges
+                       out-edges-by-subject out-offset-edges-by-subject
+                       out-edges-by-object  out-offset-edges-by-object)
+  (define (flush)
+    (flush-output out-predicates)
+    (flush-output out-edges)
+    (flush-output out-offset-edges))
+  (define predicate-count 0)
+  (define predicate=>id (hash))
+  (define subject=>edges (hash))
+  (define object=>edges (hash))
+  (define add-edge
+    (let ((id 0))
+      (define (add-predicate predicate)
+        (cond ((not (hash-has-key? predicate=>id predicate))
+               (set! predicate=>id
+                 (hash-set predicate=>id predicate predicate-count))
+               (detail-write out-predicates #f predicate)
+               (set! predicate-count (+ predicate-count 1))
+               (- predicate-count 1))
+              (else (hash-ref predicate=>id predicate))))
+      (lambda (subject-cui object-cui props)
+        (match-define (cons subject subject-category)
+                      (hash-ref cui=>id&cat subject-cui))
+        (match-define (cons object object-category)
+                      (hash-ref cui=>id&cat object-cui))
+        (define (required-prop props key)
+          (define kv (assoc key props))
+          (if kv (cdr kv) (error "missing required property:" id key)))
+        (define type (required-prop props "type"))
+        (define pid (add-predicate type))
+        (define (other-key? kv) (not (equal? "type" (car kv))))
+        (set! subject=>edges
+          (hash-set subject=>edges subject
+                    (cons (edge->bytes
+                            (vector subject pid object-category object id))
+                          (hash-ref subject=>edges subject '()))))
+        (set! object=>edges
+          (hash-set object=>edges object
+                    (cons (edge->bytes
+                            (vector object pid subject-category subject id))
+                          (hash-ref object=>edges object '()))))
+        (detail-write out-edges out-offset-edges
+                      (vector subject pid object (filter other-key? props)))
+        (set! id (+ id 1)))))
+
+  (define edges (csv-records in-edge))
+  (define edgeprops (csv-records in-edgeprop))
+  (define first-edge (edges 'next))
+  (define first-edgeprop (edgeprops 'next))
+  (when (not (and first-edge first-edgeprop)) (error "empty edge files"))
+  (match-define (list eid0 subject-cui object-cui) first-edge)
+  (match-define (list eid key val) first-edgeprop)
+  (when (not (equal? eid0 eid)) (error "mismatching edge ids:" eid0 eid))
+
+  (let loop ((count 1) (current-eid eid) (subject-cui subject-cui)
+                       (object-cui object-cui))
+    (define props
+      (let loop-props ((row (edgeprops 'current)))
+        (match row
+          ((list (? (lambda (eid) (equal? current-eid eid))) key val)
+           (cons (cons key val) (loop-props (edgeprops 'next))))
+          (_ '()))))
+    (add-edge subject-cui object-cui props)
+    (when (= 0 (remainder count 100000))
+      (printf "Processed ~s edges\n" count)
+      (flush))
+
+    (match (cons (edges 'next) (edgeprops 'current))
+      ((cons (list eid0 subject-cui object-cui) (list eid _ _))
+       (when (not (equal? eid0 eid)) (error "mismatching edge ids:" eid0 eid))
+       (loop (+ 1 count) eid subject-cui object-cui))
+      ((cons #f #f)
+       (printf "Found ~s edges involving ~s predicates\n"
+               (- count 1) predicate-count)
+       (flush)
+       (define max-edges/src 0)
+       (for ((src-id (in-range 0 (hash-count cui=>id&cat))))
+            (when (= 0 (remainder src-id 10000))
+              (printf "Processed edges for ~s concepts\n" src-id)
+              (flush-output out-edges-by-subject)
+              (flush-output out-offset-edges-by-subject)
+              (flush-output out-edges-by-object)
+              (flush-output out-offset-edges-by-object))
+            (define (write-edges-by-X X=>edges out out-offset)
+              (define edges/src 0)
+              (for ((edge (sort (hash-ref X=>edges src-id '()) bytes<?)))
+                   (set! edges/src (+ edges/src 1))
+                   (detail-write out #f edge)
+                   (offset-write out-offset (file-position out)))
+              (when (< max-edges/src edges/src)
+                (printf "Max edges per source concept seen for ~s: ~s\n"
+                        src-id edges/src)
+                (set! max-edges/src edges/src)))
+            (write-edges-by-X
+              subject=>edges out-edges-by-subject out-offset-edges-by-subject)
+            (write-edges-by-X
+              object=>edges out-edges-by-object out-offset-edges-by-object)))
+      (_ (error "malformed row(s):" (edges 'current) (edgeprops 'current))))))
+
 (define (call-with-?-files cw?f paths proc)
   (let loop ((paths paths) (ports '()))
     (if (null? paths) (apply proc (reverse ports))
@@ -129,5 +231,21 @@
                                   (fname-offset fnout-concepts-by-category)))
             (lambda out* (apply process-nodes in-nodeprop out*))))))
 
-;; TODO:
-;(printf "\nProcessing edges:\n")
+(printf "\nProcessing edges:\n")
+(time (call-with-input-file
+        (graph-path fnin-edge)
+        (lambda (in-edge)
+          (validate-header edge-header-expected in-edge)
+          (call-with-input-file
+            (graph-path fnin-edgeprop)
+            (lambda (in-edgeprop)
+              (validate-header edgeprop-header-expected in-edgeprop)
+              (call-with-output-files
+                (map graph-path (list fnout-predicates
+                                      fnout-edges (fname-offset fnout-edges)
+                                      fnout-edges-by-subject
+                                      (fname-offset fnout-edges-by-subject)
+                                      fnout-edges-by-object
+                                      (fname-offset fnout-edges-by-object)))
+                (lambda out*
+                  (apply process-edges in-edge in-edgeprop out*))))))))
