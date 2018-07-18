518963e6b5d0502563e450022b178b31c89e3fc5
William Byrd <webyrd@gmail.com>
2018-07-18 12:00:32 -0500

    porting tacrine example to new data/query format

--- biolink/example-tacrine-semmed.rkt
+++ biolink/example-tacrine-semmed.rkt
@@ -29,3 +29,12 @@
                                              'no-type-found)))
                                (list id name type)))]))
                       (run* (i v) (db:~name-concepto semmed "tacrine" i v)))))
+;; =>
+'(("UMLS:C0386973" "6-chlorotacrine" "['Organic Chemical']")
+  ("UMLS:C0295380" "2-hydroxytacrine" "['Pharmacologic Substance', 'Organic Chemical']")
+  ("UMLS:C0039245" "Tacrine" "['Pharmacologic Substance', 'Organic Chemical']")
+  ("UMLS:C1435294" "N-butyramide-tacrine" "['Pharmacologic Substance', 'Organic Chemical']")
+  ("UMLS:C0771182" "Tacrine Hydrochloride" "['Pharmacologic Substance', 'Organic Chemical']")
+  ("UMLS:C0659809" "N-methyltacrine" "['Organic Chemical']")
+  ("UMLS:C0295379" "4-hydroxytacrine" "['Pharmacologic Substance', 'Organic Chemical']")
+  ("UMLS:C0099894" "7-methoxytacrine" "['Pharmacologic Substance', 'Organic Chemical']"))
