a546b1d3eb5abacaf65c6ec31be3b2abb9c7b865
gregr <greg.weiqi@gmail.com>
2018-08-01 16:44:27 -0400

    Load all four dbs in example-full.rkt

--- biolink/example-full.rkt
+++ biolink/example-full.rkt
@@ -7,11 +7,17 @@
 (define semmed (time (make-db "data/semmed")))
 (displayln "loading monarch-lite")
 (define monarch (time (make-db "data/monarch-lite")))
+(displayln "loading rtx")
+(define rtx (time (make-db "data/rtx")))
+(displayln "loading scigraph")
+(define scigraph (time (make-db "data/scigraph")))
 
 (define (edgeo e)
   (conde
     ((fresh (ee) (== `(semmed . ,ee) e) (db:edgeo semmed ee)))
-    ((fresh (ee) (== `(monarch . ,ee) e) (db:edgeo monarch ee)))))
+    ((fresh (ee) (== `(monarch . ,ee) e) (db:edgeo monarch ee)))
+    ((fresh (ee) (== `(rtx . ,ee) e) (db:edgeo rtx ee)))
+    ((fresh (ee) (== `(scigraph . ,ee) e) (db:edgeo scigraph ee)))))
 
 (newline)
 (displayln "some edges:")
