c22917675fb9b15e00aff1da756a4783d1c51f91
William Byrd <webyrd@gmail.com>
2018-08-16 17:19:06 -0500

    added code to calculate concepts, and overlap between concepts, for different data sources

---
+++ biolink/examine-concepts.rkt
@@ -0,0 +1,54 @@
+#lang racket
+
+(provide
+ 
+ alist-of-concept-lists
+ alist-of-concept-sets
+ concept-tag-intersections
+ )
+
+
+(define get-concepts-in-db
+  (lambda (db)
+    (printf "reading ~s concept tags...\n" db)
+    (let ((p (open-input-file (string-append "data/" db "/concepts.scm"))))
+      (let loop ((concept (read p))
+                 (concept-sources '()))
+        (cond
+          [(eof-object? concept)       
+           (close-input-port p)
+           (printf "read ~s ~s concept tags\n" (length concept-sources) db)
+           (sort concept-sources string<?)]
+          [else (let* ((concept-tag (vector-ref concept 0))
+                       (tag-parts (regexp-split #rx":" concept-tag))
+                       (tag-source (car tag-parts))
+                       (concept-sources (if (member tag-source concept-sources)
+                                            concept-sources
+                                            (cons tag-source concept-sources))))
+                  (loop (read p) concept-sources))])))))
+
+(define alist-of-concept-lists
+  (map (lambda (db)
+         (cons db (get-concepts-in-db db)))
+       '("monarch-lite" "rtx" "scigraph" "semmed")))
+
+(define alist-of-concept-sets
+  (map (lambda (e)
+         (cons (car e) (apply set (cdr e))))
+       alist-of-concept-lists))
+
+;; calculate intersections (overlaps) between data sources
+(define concept-tag-intersections
+  (let loop ((alist-of-concept-sets alist-of-concept-sets)
+             (intersections '()))
+    (match alist-of-concept-sets
+      ['() intersections]
+      [`((,db1 . ,concept-set1) . ,rest)
+       (loop (cdr alist-of-concept-sets)
+             (append (map (lambda (e)
+                            (match e
+                              [`(,db2 . ,concept-set2)
+                               (list (list db1 db2)
+                                     (sort (set->list (set-intersect concept-set1 concept-set2)) string<?))]))
+                          rest)
+                     intersections))])))
