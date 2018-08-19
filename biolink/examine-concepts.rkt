bb585ebbb4ebb9b9a54c302c20f8e971718eedbd
William Byrd <webyrd@gmail.com>
2018-08-19 18:06:51 -0500

    added some simple concept count expressions

--- biolink/examine-concepts.rkt
+++ biolink/examine-concepts.rkt
@@ -46,12 +46,18 @@
 (let ((monarch-lite-concept-counts (hash->list (caddr (assoc "monarch-lite" concept-info))))
       (scigraph-concept-counts (hash->list (caddr (assoc "scigraph" concept-info)))))
   (let ((scigraph-concept-counts-minus-semmed (remove (assoc "UMLS" scigraph-concept-counts) scigraph-concept-counts)))
-    (equal? monarch-lite-concept-counts
-            scigraph-concept-counts-minus-semmed)))
+    (let ((same (equal? monarch-lite-concept-counts
+                        scigraph-concept-counts-minus-semmed)))
+      (printf "** is scigraph just monarch-lite + semmeddb? ~s\n" same)
+      same)))
 
 ;; how many concepts are there in the scigraph version of semmeddb, versus the cleaned up semmeddb?
+;;
+;; the semmeddb in scigraph is much larger than the cleaned up semmeddb
 (let ((scigraph-semmed-UMLS-concept-coount (hash-ref (caddr (assoc "scigraph" concept-info)) "UMLS"))
       (cleand-up-esemmed-UMLS-concept-coount (hash-ref (caddr (assoc "semmed" concept-info)) "UMLS")))
+  (printf "** semmed UMLS concepts in scigraph: ~s\n" scigraph-semmed-UMLS-concept-coount)
+  (printf "** semmed UMLS concepts in cleaned up semmeddb: ~s\n" cleand-up-esemmed-UMLS-concept-coount)
   (list scigraph-semmed-UMLS-concept-coount
         cleand-up-esemmed-UMLS-concept-coount))
 
