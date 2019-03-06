078f8db443b349e03e3e428d047968c7764fb7e9
gregr <greg.weiqi@gmail.com>
2019-03-06 11:21:57 -0500

    Generalize workflow-web-server.rkt a bit more

--- biolink/workflow-web-server.rkt
+++ biolink/workflow-web-server.rkt
@@ -34,44 +34,94 @@
                                  (filter essence? nodes)))))
        (hash-ref jsdata 'result_list)))
 
+(define (json->all-cuis jsdata)
+  (map (lambda (named-cui)
+         ;(define name (cadr jsdata))
+         (car named-cui))
+       jsdata))
+
 (define reasoner-id "mediKanren-0.00000000000000zerozero...")
+
+(define rem-dups
+  (lambda (ls)
+    (cond
+      [(null? ls) '()]
+      [(member (car ls) (cdr ls)) (rem-dups (cdr ls))]
+      [else (cons (car ls) (rem-dups (cdr ls)))])))
+
+(define (edge->json edge)
+  (define db (car edge))
+  (define type (cdr (cadddr (cdr edge))))
+  (define src (cadr (caddr edge)))
+  (define tgt (cadr (cadddr edge)))
+  (define eprops (cddddr (cdr edge)))
+  (define kpmids (assoc "pmids" eprops))
+  (define kpubs (assoc "publications" eprops))
+  (define pmids (rem-dups (if kpmids (cdr kpmids) '())))
+  (define pubs (rem-dups (if kpubs (cdr kpubs) '())))
+  (define publications
+    (append (map (lambda (id)
+                   (string-append "https://www.ncbi.nlm.nih.gov/pubmed/" id))
+                 pmids) pubs))
+  (hash 'type           type
+        'source_id      src
+        'target_id      tgt
+        'provided_by    db
+        'confidence     0.00000000000000001
+        'publications   publications
+        'attribute_list '()))
 
 (define (concept->json concept)
   (define cui (list-ref concept 2))
   (define name (list-ref concept 3))
+  ;; TODO: more general type.
+  (define type "chemical_substance")
   (define props (list-tail concept 4))
   (define description (cdr (or (assoc "description" props) '(#f . ""))))
   (define uri         (cdr (or (assoc "uri"         props) '(#f . ""))))
-  (define node
-    (hash 'description     description
-          'id              cui
-          'name            name
-          'node_attributes '()
-          'type            "chemical_substance"
-          'uri             uri))
-  (hash 'confidence  0.00000000000000001
-        'description ""
-        'essence     name
-        'id          ""
-        'reasoner_id reasoner-id
-        'result_graph (hash 'edge_list '()
-                            'node_list (list node))
-        'result_type                   ""
-        'row_data                      '()
-        'text                          (string-append "Something about: " name)))
+  (hash 'description     description
+        'id              cui
+        'name            name
+        'type            type
+        'uri             uri
+        'node_attributes '()))
 
-(define (concepts->json concepts)
+(define (result->json result)
+  ;; TODO:
+  (define essence    "")
+  (define confidence 0.00000000000000001)
+  (define concepts   (cdr (assoc 'concepts result)))
+  (define edges      (cdr (assoc 'edges result)))
+  (hash 'confidence   confidence
+        'description  ""
+        'essence      essence
+        'id           ""
+        'reasoner_id  reasoner-id
+        'result_graph (hash 'edge_list (map edge->json    edges)
+                            'node_list (map concept->json concepts))
+        'result_type  "answer"
+        'row_data     '()
+        'text         ""))
+
+(define (response->json results)
+  (define (pad n) (~a n #:width 2 #:align 'right #:pad-string "0"))
+  (define datetime
+    (let ((dt (seconds->date (current-seconds) #f)))
+      (format
+        "~a-~a-~a ~a:~a:~a"
+        (date-year dt) (pad (date-month dt)) (pad (date-day dt))
+        (pad (date-hour dt)) (pad (date-minute dt)) (pad (date-second dt)))))
   (hash 'context                "https://raw.githubusercontent.com/biolink/biolink-model/master/context.jsonld"
-        'datetime               "2018-09-20 20:45:05"
+        'datetime               datetime
         'id                     ""
         'message                ""
-        'n_results              1
+        'n_results              (length results)
         'original_question_text ""
         'query_type_id          ""
         'reasoner_id            reasoner-id
         'response_code          "OK"
         'restated_question_text ""
-        'result_list            (map concept->json concepts)
+        'result_list            (map result->json results)
         'schema_version         "0.8.0"
         'table_column_names     '()
         'terms                  (hash 'disease "")
@@ -82,8 +132,13 @@
   (define cuis (json->cuis jsdata))
   (displayln `(cuis-received: ,cuis))
   (define concepts (append* (map workflow1module2 cuis)))
-  (concepts->json concepts))
+  (response->json (list (list (cons 'concepts concepts) (cons 'edges '())))))
 
+(define (direct-query.json jsdata)
+  (define cuis (json->all-cuis jsdata))
+  (displayln `(cuis-received: ,cuis))
+  (define concepts (append* (map workflow1module2 cuis)))
+  (response->json (list (list (cons 'concepts concepts) (cons 'edges '())))) )
 
 (define mime:html (string->bytes/utf-8 "text/html; charset=utf-8"))
 (define mime:json (string->bytes/utf-8 "application/json; charset=utf-8"))
@@ -103,6 +158,12 @@
              (query.json
                (bytes->jsexpr (request-post-data/raw req))))))
 
+(define (direct-query req)
+  (respond 200 "OK" '() mime:json
+           (jsexpr->string
+             (direct-query.json
+               (bytes->jsexpr (request-post-data/raw req))))))
+
 (define (not-found req)
   (respond 404 "Not Found" '() mime:html
            (xexpr->html-string (not-found.html
@@ -115,9 +176,10 @@
 
 (define-values (dispatcher _)
   (dispatch-rules
-    (("")      #:method         "get" index)
-    (("query") #:method (regexp ".*") query)
-    (else                             not-found)))
+    (("")             #:method         "get" index)
+    (("query")        #:method (regexp ".*") query)
+    (("direct-query") #:method (regexp ".*") direct-query)
+    (else                                    not-found)))
 
 ;; after loading this file, launch the server by evaluating
 (serve/servlet dispatcher
