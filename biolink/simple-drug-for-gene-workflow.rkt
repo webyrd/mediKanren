4368cd559c0d1e6ee424bb808424696f9de9410b
William Byrd <webyrd@gmail.com>
2019-05-19 21:57:31 -0500

    working on workflow

--- biolink/simple-drug-for-gene-workflow.rkt
+++ biolink/simple-drug-for-gene-workflow.rkt
@@ -86,7 +86,7 @@
 (define ROBOKOP_EQUIVALENT_IDENTIFIERS_KEY "equivalent_identifiers")
 
 
-(define VERSION_STRING "mediKanren Simple Drug-for-Gene Workflow (Marvin Prototype) 0.1.0")
+(define VERSION_STRING "mediKanren Marvin Prototype 0.1.0 (Simple Drug-for-Gene Workflow Module)")
 (displayln VERSION_STRING)
 
 (displayln "loading HGNC-ID-to-concepts hashtable")
