75e2b0620cd334088f7893a8b96b9f70c25c6d43
gregr <greg.weiqi@gmail.com>
2019-06-03 17:50:23 -0400

    Create test-common.rkt

---
+++ biolink/test-common.rkt
@@ -0,0 +1,50 @@
+#lang racket
+(require "common.rkt")
+
+(define (decreases? p)
+  (member (cddr p) '("negatively_regulates"
+                     "prevents"
+                     "treats"
+                     "disrupts"
+                     "increases_degradation_of"
+                     "decreases_activity_of"
+                     "decreases_expression_of")))
+(define (increases? p)
+  (member (cddr p) '("positively_regulates"
+                     "produces"
+                     "causes"
+                     "causes_condition"
+                     "causally_related_to"
+                     "contributes_to"
+                     "gene_associated_with_condition"
+                     "gene_mutations_contribute_to"
+                     "decreases_degradation_of"
+                     "increases_activity_of"
+                     "increases_expression_of")))
+
+(define S (take (find-concepts #t #f 0 #f  (list "imatin")) 1))
+(define O (take (find-concepts #f #t 0 #f  (list "asthma")) 1))
+
+(define SP (map (lambda (sp)
+                  (define c (car sp))
+                  (define pS (cadr sp))
+                  (list c (filter decreases? pS) #f))
+                (find-predicates/concepts #t #f S)))
+(define OP (map (lambda (sp)
+                  (define c (car sp))
+                  (define pO (caddr sp))
+                  (list c #f (filter increases? pO)))
+                (find-predicates/concepts #f #t O)))
+
+(define X (find-Xs SP OP))
+
+(define x-bcr (car (filter (lambda (x) (equal? (cadddr (car x)) "BCR gene")) X)))
+
+(displayln 'X:)
+(pretty-print (car x-bcr))
+(newline)
+(displayln 'S-edges:)
+(pretty-print (cadr x-bcr))
+(newline)
+(displayln 'O-edges:)
+(pretty-print (caddr x-bcr))
