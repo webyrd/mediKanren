7470da4ac84bdbde69154efd2c387b4b21141760
gregr <greg.weiqi@gmail.com>
2020-03-17 15:51:18 -0400

    Provide synonymizer.rkt from query.rkt

--- biolink/pieces-parts/query.rkt
+++ biolink/pieces-parts/query.rkt
@@ -4,7 +4,8 @@
          positively-regulates negatively-regulates drug-safe
          gene drug disease phenotype
          (all-defined-out)
-         (all-from-out "../common.rkt" "../mk-db.rkt" "propagator.rkt"))
+         (all-from-out "../common.rkt" "../mk-db.rkt"
+                       "propagator.rkt" "synonymize.rkt"))
 (require "../common.rkt" "../mk-db.rkt" "propagator.rkt" "synonymize.rkt"
          racket/list (except-in racket/match ==) racket/pretty
          racket/set racket/string)
