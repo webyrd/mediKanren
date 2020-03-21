83363a7254322f1d2f05fe894e13010756846a8c
William Byrd <webyrd@gmail.com>
2020-03-21 17:50:10 -0500

    debugging

--- biolink/pieces-parts/gene-budging.rkt
+++ biolink/pieces-parts/gene-budging.rkt
@@ -2,7 +2,6 @@
 (provide (all-defined-out))
 (require "query.rkt")
 
-
 (define make-directly-regulate-gene
   (lambda (regulation-predicates)
     (lambda (gene-curie)
@@ -28,14 +27,17 @@
 
 (define curie-to-anything
   (lambda (curie predicate*)
-    (query/graph
-      (;; concepts
-       (X curie)
-       (T #f))
-      ;; edges
-      ((X->T predicate*))
-      ;; paths      
-      (X X->T T))))
+    ;;(printf "starting curie-to-anything with curie ~s and preds ~s\n" curie predicate*)
+    (let ((val (query/graph
+                ( ;; concepts
+                 (X curie)
+                 (T #f))
+                ;; edges
+                ((X->T predicate*))
+                ;; paths      
+                (X X->T T))))
+      ;;(printf "finished curie-to-anything with curie ~s and preds ~s\n" curie predicate*)
+      val)))
 
 (define curie-to-tradenames
   (lambda (curie)
@@ -65,19 +67,24 @@
 
 (define pubmed-URLs-from-bogo-edge
   (lambda (bogo-edge)
+    ;;(printf "starting pubmed-URLs-from-bogo-edge\n")
     (define concrete-edges (list-ref bogo-edge 2))
-    (remove-duplicates
-      (append*
-        (map pubmed-URLs-from-edge concrete-edges)))))
+    ;;(printf "concrete-edges length = ~s\n" (length concrete-edges))
+    (let ((url-ls (map pubmed-URLs-from-edge concrete-edges)))
+      ;;(printf "url-ls = ~s\n" url-ls)
+      (remove-duplicates (append* url-ls)))))
 
 
 (define drug-info-for-curie
   (lambda (curie)
+    ;;(printf "starting drug-info-for-curie\n")
     (map
      (lambda (l)
        (match l
-         [`(,name . ,a)
-          (cons name (map curie-synonyms/names (curies/query a 'T)))]))
+         [`(,name . ,q)
+          (let ((ls (map curie-synonyms/names (curies/query q 'T))))
+            (printf "ls length = ~s\n" (apply + (map length ls)))
+            (cons name ls))]))
      (list 
       (cons 'tradenames (curie-to-tradenames curie))
       (cons 'clinical-trials (curie-to-clinical-trials curie))
@@ -86,18 +93,26 @@
 
 
 (define drug-info-from-bogo-edge
-  (lambda (bogo-edge)
+  (lambda (bogo-edge)    
     (define curie (caar bogo-edge))
+    ; (printf "curie = ~s\n" curie)
     (define pubmed-URLs (pubmed-URLs-from-bogo-edge bogo-edge))
+    ; (printf "calculating curie-synonyms/names\n")
+    (define synonyms/names (curie-synonyms/names curie))
+    ; (printf "calculated curie-synonyms/names of length ~s\n" (length synonyms/names))
     (append
      (list (cons 'curie curie))
-     (list (cons 'curie-synonyms/names (curie-synonyms/names curie)))
+     (list (cons 'curie-synonyms/names synonyms/names))
      (drug-info-for-curie curie)
      (list (cons 'pubmeds pubmed-URLs)))))
 
 
+#|
+(pubmed-URLs-from-edge '(semmed 907404 (4417 "UMLS:C0023870" "Lithium" (4 . "chemical_substance") (("umls_type_label" . "(\"Pharmacologic Substance\" \"Element, Ion, or Isotope\")") ("xrefs" . "(\"SNOMEDCT_US:85899009\" \"LNC:LP16175-9\" \"LCH_NW:sh85077577\" \"USPMG:MTHU000811\" \"NCI_NCI-GLOSS:CDR0000476347\" \"MMSL:d00061\" \"MTHSPL:NOCODE\" \"CPM:32027\" \"UNII:9FN79X2M3F\" \"MESH:D008094\" \"SNOMEDCT_US:321719003\" \"PDQ:CDR0000039491\" \"NDFRT:N0000147892\" \"SNM:F-10380\" \"VANDF:4019803\" \"MEDCIN:199593\" \"RCD:XM0lB\" \"SNMI:C-15400\" \"CHV:0000007490\" \"NDDF:001532\" \"RCD:d6...\" \"NDFRT:N0000166224\" \"MTHSPL:9FN79X2M3F\" \"ATC:N05AN01\" \"DRUGBANK:DB01356\" \"LNC:MTHU004197\" \"AOD:0000019454\" \"PSY:28590\" \"CSP:1852-6098\" \"MTH:U002050\" \"LCH:U002722\" \"INCHIKEY:SIAPCJWMELPYOE-UHFFFAOYSA-N\" \"RXNORM:6448\" \"CHEMBL:CHEMBL2146126\" \"NCI:C95186\" \"NCI:C1318\")") ("id" . "UMLS:C0023870") ("umls_type" . "(\"T121\" \"T196\")"))) (7571 "UMLS:C1428785" "KDM1A gene" (2 . "gene") (("umls_type_label" . "(\"Gene or Genome\")") ("xrefs" . "(\"NCI_NCI-HGNC:HGNC:29079\" \"HGNC:HGNC:29079\" \"OMIM:609132\" \"NCI:C78141\" \"MTH:NOCODE\")") ("id" . "UMLS:C1428785") ("umls_type" . "(\"T028\")"))) (10 . "negatively_regulates") (("is_defined_by" . "semmeddb") ("negated" . "False") ("SEMMED_PRED" . "INHIBITS") ("pmids" . "21727907") ("provided_by" . "semmeddb_sulab") ("n_pmids" . "1") ("relation" . "semmeddb:negatively_regulates"))))
+|#
 
 
+(printf "starting upregulation...\n")
 
 ;; kdm1a
 (define kdm1a-directly-up (directly-upregulate-gene "HGNC:29079"))
@@ -110,8 +125,22 @@
 
 (define kdm1a-directly-up-drug-info (map drug-info-from-bogo-edge edges/X->kdm1a-directly-up))
 
+#|
+;; aurkb
+(define aurkb-directly-up (directly-upregulate-gene "HGNC:11390"))
+;; returns the set of all query results (for X, for gene, for edges X->my-gene, etc.)
+
+(define aurkb-directly-up-Xs (curies/query aurkb-directly-up 'X))
+
+;; each edge corresponds to an X in aurkb-Xs
+(define edges/X->aurkb-directly-up (edges/ranked (ranked-paths aurkb-directly-up) 0 0))
+
+(define aurkb-directly-up-drug-info (map drug-info-from-bogo-edge edges/X->aurkb-directly-up))
+|#
 
 #|
+(printf "starting downregulation...\n")
+
 (define kdm1a-directly-down (directly-downregulate-gene "HGNC:29079"))
 ;; returns the set of all query results (for X, for gene, for edges X->my-gene, etc.)
 
@@ -120,6 +149,16 @@
 ;; each edge corresponds to an X in kdm1a-Xs
 (define edges/X->kdm1a-directly-down (edges/ranked (ranked-paths kdm1a-directly-down) 0 0))
 
-;; Does this diverge??? Or is it just slow???
+;; 
 (define kdm1a-directly-down-drug-info (map drug-info-from-bogo-edge edges/X->kdm1a-directly-down))
 |#
+
+
+
+;;; 2-hop
+
+;; ACE2   HGNC:13557
+
+;; CTSL   HGNC:2537
+
+;; include semmed sentences, when possible  (date would be nice)
