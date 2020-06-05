becb3e4527bfa203e53a81bbf6c0e08e9545f706
gregr <greg.weiqi@gmail.com>
2020-06-05 11:40:37 -0400

    Provide curie-synonyms-raw

--- biolink/pieces-parts/synonymize.rkt
+++ biolink/pieces-parts/synonymize.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (provide HGNC-CURIE->synonymized-concepts
-         curie-aliases curie-synonyms curie->name curie->concepts
-         curie-synonyms/names
+         curie-aliases curie-synonyms curie-synonyms-raw curie-synonyms/names
+         curie->name curie->concepts
          (all-from-out "../common.rkt" "../mk-db.rkt"))
 (require "../common.rkt" "../mk-db.rkt" "../db.rkt" racket/runtime-path)
 
