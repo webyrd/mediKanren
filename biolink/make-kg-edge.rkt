8288cb3564c0ac5675ea63ddf865cc604dcd5890
gregr <greg.weiqi@gmail.com>
2020-07-02 15:43:00 -0400

    Replace tab indentation with spaces

--- biolink/make-kg-edge.rkt
+++ biolink/make-kg-edge.rkt
@@ -11,8 +11,8 @@ name-file.edge-props.scm
 :ID propname value
 
 ex-row: 1 edge_label biolink:has_gene_product
-	1 provided_by blah
-	1
+  1 provided_by blah
+  1
 |#
 
 (define directory-path
@@ -36,21 +36,21 @@ ex-row: 1 edge_label biolink:has_gene_product
 (let* ((header (read-line input-edges))
        (header (string-split header "\t")))
   (let loop ((i 0)
-	     (line-str (read-line input-edges)))
+             (line-str (read-line input-edges)))
     (cond
       ((eof-object? line-str)
        (close-input-port input-edges)
        (close-output-port edges-export-file)
        (close-output-port edge-props-export-file))
       (else
-       (let ((line (string-split line-str "\t")))
-	 (fprintf edges-export-file "~a\t~a\t~a\n" i (car line) (cadr line))
-	 (let loop-inner ((props (cddr line))
-			  (headers (cddr header)))
-	   (when (not (null? props))
-	     (unless (string=? "" (car props))
-	       (fprintf edge-props-export-file "~a\t~a\t~s\n" i (car headers) (car props)))
-	     (loop-inner (cdr props) (cdr headers)))))
-       (loop
-	(+ 1 i)
-	(read-line input-edges))))))
+        (let ((line (string-split line-str "\t")))
+          (fprintf edges-export-file "~a\t~a\t~a\n" i (car line) (cadr line))
+          (let loop-inner ((props (cddr line))
+                           (headers (cddr header)))
+            (when (not (null? props))
+              (unless (string=? "" (car props))
+                (fprintf edge-props-export-file "~a\t~a\t~s\n" i (car headers) (car props)))
+              (loop-inner (cdr props) (cdr headers)))))
+        (loop
+          (+ 1 i)
+          (read-line input-edges))))))
