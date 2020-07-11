21e863e9dde1ff6446816dd71d76d4724336520a
gregr <greg.weiqi@gmail.com>
2020-07-10 22:02:44 -0400

    Define common paths

--- biolink2/common.rkt
+++ biolink2/common.rkt
@@ -1,5 +1,13 @@
 #lang racket/base
 (provide
   (all-from-out "dbk/dbk.rkt")
+  path/data path/root path.data path.root
   )
-(require "dbk/dbk.rkt")
+(require "dbk/dbk.rkt" racket/runtime-path)
+
+(define-runtime-path path.root ".")
+(define (path/root relative-path)
+  (path->string (build-path path.root relative-path)))
+(define path.data (path/root "data"))
+(define (path/data relative-path)
+  (path->string (build-path path.data relative-path)))
