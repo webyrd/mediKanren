7188cf6224adbe2efa52b5b00c95ef59ed5c500f
gregr <greg.weiqi@gmail.com>
2020-07-10 23:18:38 -0400

    Define materialize-relation

--- biolink2/common.rkt
+++ biolink2/common.rkt
@@ -8,10 +8,13 @@
   read/file read/string
 
   validate-header
+  materialize-relation
   )
 (require
   "dbk/dbk.rkt"
   racket/list racket/port racket/runtime-path racket/string)
+
+(define buffer-size 100000)  ;; main memory used for external sorting
 
 (define-runtime-path path.root ".")
 (define (path-simple path) (path->string (simplify-path path)))
@@ -59,3 +62,33 @@
   (define header-found (read-line in 'any))
   (when (not (equal? header-found (string-join header-expected delimiter)))
     (error "unexpected header:" header-found header-expected)))
+
+(define (materialize-stream stream mat-args)
+  (let ((mat (materializer mat-args)))
+    (define count 0)
+    (time (s-each (lambda (x)
+                    (when (= 0 (remainder count 100000))
+                      (printf "Ingested ~s rows\n" count))
+                    (mat 'put x)
+                    (set! count (+ count 1)))
+                  stream))
+    (printf "Processing ~s rows\n" count)
+    (time (mat 'close))
+    (printf "Finished processing ~s rows\n" count)))
+
+(define (materialize-relation path fnin header delimiter port->stream transform
+                              fields types indexes)
+  (unless (directory-exists? (path/data path))
+    (printf "materializing relation ~s from ~s\n"
+            (path/data path) (path/data fnin))
+    (let/files ((in (path/data fnin))) ()
+      (when header (validate-header in header delimiter))
+      (define stream (port->stream in))
+      (materialize-stream
+        (if transform (s-map transform stream) stream)
+        `((buffer-size     . ,buffer-size)
+          (path            . ,(path/data path))
+          (attribute-names . ,fields)
+          (attribute-types . ,types)
+          (tables  ((columns . ,fields)))
+          (indexes . ,(map (lambda (i) (list (cons 'columns i))) indexes)))))))
