74824e67405a25d664b9e3037380cb7861c25a07
19katz <katherinezhang@college.harvard.edu>
2020-08-18 23:58:05 -0700

    adding new code

--- biolink/pieces-parts/make-map.rkt
+++ biolink/pieces-parts/make-map.rkt
@@ -17,10 +17,24 @@
   (foldl
    (lambda (e s)
      (let ((subject-prefix (concept->prefix (edge->subject e)))
-           (object-prefix (concept->prefix (edge->object e)))
-           (predicate (cdr (edge->pred e))))
-       (set-add s (list subject-prefix predicate object-prefix))))
+           (predicate (cdr (edge->pred e)))
+           (object-prefix (concept->prefix (edge->object e))))
+       (set-add s (list subject-prefix object-prefix predicate))))
    (set) result))
 
+
+(define out (open-output-file "orange_graph.dot" #:exists 'replace))
+(fprintf out "digraph{\ngraph [ bgcolor=lightgray, fontname=Arial, fontcolor=blue, 
+                         fontsize=12 ];\n
+                 node [ fontname=Arial, fontcolor=blue, fontsize=11];\n
+                 edge [ fontname=Helvetica, fontcolor=red, fontsize=10, labeldistance=2, labelangle=-50 ];\n
+    splines=\"FALSE\";\n
+    rankdir=\"LR\";\n")
+(set-for-each
+ graph
+ (lambda (t)
+   (apply fprintf out "\t~s -> ~s [label=~s]\n" t)))
+(fprintf out "}\n")
+(close-output-port out)
 
 
