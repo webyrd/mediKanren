62e95229c9f0342d311721f276648742304518f1
TriageDr <mjpatton@uab.edu>
2020-09-19 17:11:19 -0500

    added new json query function and sample queries for chembl specific api calls to molepro

--- biolink/open-api/api-query.rkt
+++ biolink/open-api/api-query.rkt
@@ -10,6 +10,14 @@
 ;; Broad Institute KP
 (define url.broad
   "https://translator.broadinstitute.org/molepro_reasoner")
+
+;; Broad Institute KP specific for CHEMBL queries
+(define url.broad_chembl
+  "https://translator.broadinstitute.org/chembl")
+
+(define chembl-drug-indications/transform
+  "/indications/transform")
+
 (define url.unsecret
   "https://unsecret.ncats.io")
 (define path.predicates
@@ -43,51 +51,51 @@
 
 (module+ main
   ;; test predicates available on Broad Institute KG
-  ;(pretty-print
-    ;(api-query (string-append url.broad path.predicates)))
+ #;(pretty-print
+   (api-query (string-append url.broad path.predicates)))
 
-  ;(pretty-print
-    ;(api-query (string-append url.broad path.query)
-               ;(js-query (list (hash 'id        "e00"
-                                     ;'source_id "n00"
-                                     ;'target_id "n01"
-                                     ;'type      "affects"))
-                         ;(list (hash 'curie "CID:2244"
-                                     ;'id    "n00"
-                                     ;'type  "chemical_substance")
-                               ;(hash 'id    "n01"
-                                     ;'type  "gene")))))
+  #;(pretty-print
+     (api-query (string-append url.broad path.query)
+              (js-query (list (hash 'id        "e00"
+                                    'source_id "n00"
+                                    'target_id "n01"
+                                    'type      "affects"))
+                        (list (hash 'curie "CID:2244"
+                                    'id    "n00"
+                                    'type  "chemical_substance")
+                              (hash 'id    "n01"
+                                    'type  "gene")))))
 
-  ;(js-count
-    ;(time (api-query (string-append url.unsecret path.predicates))))
+  #;(js-count
+    (time (api-query (string-append url.unsecret path.predicates))))
 
-  ;(pretty-print
-    ;(time
-      ;(api-query (string-append url.unsecret path.query)
-                 ;(js-query (list (hash 'id        "e00"
-                                       ;'source_id "n00"
-                                       ;'target_id "n01"
-                                       ;'type      "causes"))
-                           ;(list (hash 'curie "UMLS:C0004096"
-                                       ;'id    "n01"
-                                       ;'type  "disease")
-                                 ;(hash 'id    "n00"
-                                       ;'type  "gene"))))))
+  #;(pretty-print
+    (time
+      (api-query (string-append url.unsecret path.query)
+                (js-query (list (hash 'id        "e00"
+                                       'source_id "n00"
+                                       'target_id "n01"
+                                       'type      "causes"))
+                           (list (hash 'curie "UMLS:C0004096"
+                                       'id    "n01"
+                                       'type  "disease")
+                                 (hash 'id    "n00"
+                                       'type  "gene"))))))
 
-  ;(pretty-print
-    ;(time
-      ;(api-query (string-append url.broad path.query)
-                 ;(js-query (list (hash 'id        "e00"
-                                       ;'source_id "n00"
-                                       ;'target_id "n01"
-                                       ;'type      "causes"))
-                           ;(list (hash 'curie "UMLS:C0004096"
-                                       ;'id    "n01"
-                                       ;'type  "disease")
-                                 ;(hash 'id    "n00"
-                                       ;'type  "gene"))))))
-
-  (pretty-print
+  #;(pretty-print
+    (time
+       (api-query (string-append url.broad path.query)
+                  (js-query (list (hash 'id        "e00"
+                                        'source_id "n00"
+                                        'target_id "n01"
+                                        'type      "causes"))
+                            (list (hash 'curie "UMLS:C0004096"
+                                        'id    "n01"
+                                        'type  "disease")
+                                  (hash 'id    "n00"
+                                        'type  "gene"))))))
+  
+  #;(pretty-print
     (time (api-query (string-append url.unsecret path.query)
                      (js-query (list (hash 'id        "e00"
                                            'source_id "n00"
@@ -99,3 +107,119 @@
                                      (hash 'id    "n01"
                                            'type  "gene"))))))
 )
+
+
+
+#|NOTES FOR SEPTEMBER 2020 RELAY API CALLS|#
+
+#|
+
+1. all "NO-evidence/provenance" queries are to the base broad.url
+"https://translator.broadinstitute.org/molepro_reasoner
+
+2. all "LIMITED evidence/provenance" queries are to the chembl url
+"https://translator.broadinstitute.org/chembl"
+
+|#
+
+;;MOLEPRO drug --treats--> disease query w/ NO EVIDENCE
+(pretty-print
+ (time (api-query (string-append url.broad path.query)
+                  (js-query (list (hash 'id        "e00"
+                                        'source_id "n00"
+                                        'target_id "n01"
+                                        'type      "treats"))
+                            (list (hash 'curie "ChEMBL:CHEMBL25"
+                                        'id    "n00"
+                                        'type  "chemical_substance")
+                                  (hash 'id    "n01"
+                                        'type  "disease"))))))
+
+;; new query structure for chembl specific provenance/evidence queries 
+(define (js-query/transform curie)
+  (hash 'collection
+        (list
+         (hash
+          'id ""
+          'identifiers
+          (hash
+           'chembl curie)))     
+        'controls '()))
+
+#|
+;; {} = hash                            ;
+;; [] = list                            ;
+;; key:value                            ;
+                                        ;
+QUERY/TRANSFORM QUERY STRUCTURE         ;
+------------------------------
+
+query = {
+    'collection': [{
+            'id':'',
+            'identifiers': {'chembl':'ChEMBL:CHEMBL25'}
+        }],
+    'controls':[]
+}
+|#
+
+;; addition provenance/evidence for drug indication query
+(pretty-print
+ (time
+  (api-query
+   (string-append (string-append url.broad_chembl chembl-drug-indications/transform))
+   (js-query/transform
+    "ChEMBL:CHEMBL25"))))
+
+
+;; STRING gene->gene edge
+(pretty-print
+ (time (api-query (string-append url.broad path.query)
+                  (js-query (list (hash 'id        "e00"
+                                        'source_id "n00"
+                                        'target_id "n01"
+                                        'type      "related_to"))
+                            (list (hash 'curie "HGNC:4556"
+                                        'id    "n00"
+                                        'type  "gene")
+                                  (hash 'id    "n01"
+                                        'type  "gene"))))))
+
+;; CMAP gene-to-compound query
+(pretty-print
+ (time (api-query (string-append url.broad path.query)
+                  (js-query (list (hash 'id        "e00"
+                                        'source_id "n00"
+                                        'target_id "n01"
+                                        'type      "correlated_with"))
+                            (list (hash 'curie "HGNC:4556"
+                                        'id    "n00"
+                                        'type  "gene")
+                                  (hash 'id    "n01"
+                                        'type  "chemical_substance"))))))
+
+;; CMAP gene-to-gene query 
+(pretty-print
+ (time (api-query (string-append url.broad path.query)
+                  (js-query (list (hash 'id        "e00"
+                                        'source_id "n00"
+                                        'target_id "n01"
+                                        'type      "correlated_with"))
+                            (list (hash 'curie "HGNC:4556"
+                                        'id    "n00"
+                                        'type  "gene")
+                                  (hash 'id    "n01"
+                                        'type  "gene"))))))
+
+;; CMAP gene-to-gene query 
+(pretty-print
+ (time (api-query (string-append url.broad path.query)
+                  (js-query (list (hash 'id        "e00"
+                                        'source_id "n00"
+                                        'target_id "n01"
+                                        'type      "correlated_with"))
+                            (list (hash 'curie "HGNC:4556"
+                                        'id    "n00"
+                                        'type  "gene")
+                                  (hash 'id    "n01"
+                                        'type  "gene"))))))
