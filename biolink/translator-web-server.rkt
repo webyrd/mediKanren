b73454bf472693c9da9c328e58b14ee766211d5b
gregr <greg.weiqi@gmail.com>
2021-01-20 19:55:28 -0500

    Create v2 endpoint

--- biolink/translator-web-server.rkt
+++ biolink/translator-web-server.rkt
@@ -87,6 +87,37 @@ query_result_clear.addEventListener('click', function(){
   clear_result();
 }, false);
 });")
+(define v2:index.js "
+window.addEventListener('load', function(){
+var query_result       = document.getElementById('query-result');
+var query_result_clear = document.getElementById('query-result-clear');
+var query_form         = document.getElementById('query-form');
+var query_text         = document.getElementById('query-text');
+var query_submit       = document.getElementById('query-submit');
+function pretty_json(json_text) {
+  try { return JSON.stringify(JSON.parse(json_text), null, 2); }
+  catch (_) { return json_text; }
+}
+function query(data) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener('load',  function(event){ show(xhr.responseText); });
+  xhr.addEventListener('error', function(event){ show('POST error'); });
+  xhr.open('POST', '/v2/query');
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+  xhr.send(data);
+}
+function show(result)   { query_result.textContent = pretty_json(result); }
+function clear_result() { query_result.textContent = ''; }
+
+query_text.textContent = pretty_json(query_text.textContent);
+query_form.addEventListener('submit', function(event){
+  event.preventDefault();
+  query(query_text.value);
+}, false);
+query_result_clear.addEventListener('click', function(){
+  clear_result();
+}, false);
+});")
 (define (not-found.html uri)
   `(html (head (title "mediKanren: 404"))
          (body (h1 "What are you looking for?")
@@ -132,6 +163,49 @@ query_result_clear.addEventListener('click', function(){
                             ))
                      (div (button ((type "submit") (id "query-submit"))
                                   "POST /query")))
+               (div (button ((id "query-result-clear")) "Clear Result"))
+               (div (pre ((id "query-result")) "Result will appear here.")))))
+
+(define v2:index.html
+  `(html (head (title "mediKanren Reasoner API")
+               (script ((src "/v2/index.js"))))
+         (body (h1 "mediKanren Reasoner API")
+               (p (a ((href "https://github.com/NCATS-Tangerine/NCATS-ReasonerStdAPI"))
+                     "NCATS Biomedical Translator Reasoners Standard API"))
+               (ul (li (a ((href "/schema.html")) "schema.html"))
+                   (li (a ((href "/schema.html2")) "schema.html2"))
+                   (li (a ((href "/schema.yaml")) "schema.yaml"))
+                   (li (a ((href "/schema.json")) "schema.json")))
+               (p (a ((href "/predicates")) "GET /predicates"))
+               (form ((method "post") (action "/v2/query") (id "query-form"))
+                     (div (textarea
+                            ((id "query-text"))
+                            "{
+  \"message\": {
+    \"query_graph\": {
+      \"nodes\": {
+        \"n0\": { \"id\": \"UMLS:C0935989\" },
+        \"n1\": { \"category\": \"gene\" },
+        \"n2\": { \"id\": \"UMLS:C0004096\" }
+      },
+      \"edges\": {
+        \"e0\": {
+          \"predicate\": \"negatively_regulates\",
+          \"subject\": \"n0\",
+          \"object\": \"n1\"
+        },
+        \"e1\": {
+          \"predicate\": \"gene_associated_with_condition\",
+          \"subject\": \"n1\",
+          \"object\": \"n2\"
+        }
+      }
+    }
+  }
+}"
+                            ))
+                     (div (button ((type "submit") (id "query-submit"))
+                                  "POST /v2/query")))
                (div (button ((id "query-result-clear")) "Clear Result"))
                (div (pre ((id "query-result")) "Result will appear here.")))))
 
@@ -316,9 +390,12 @@ query_result_clear.addEventListener('click', function(){
            (string->bytes/utf-8
              (xexpr->html-string
                (not-found.html (url->string (request-uri req)))))))
-(define (index         req)
+(define (/index req)
   (pretty-print `(request-headers: ,(request-headers req)))
   (OK req '() mime:html (xexpr->html-string index.html)))
+(define (/v2/index req)
+  (pretty-print `(request-headers: ,(request-headers req)))
+  (OK req '() mime:html (xexpr->html-string v2:index.html)))
 (define (/index.js     req) (OK req '() mime:js   index.js))
 (define (/schema.json  req) (OK req '() mime:text schema.json.txt))
 (define (/schema.yaml  req) (OK req '() mime:text schema.yaml.txt))
@@ -330,19 +407,26 @@ query_result_clear.addEventListener('click', function(){
 (define (/query        req)
   (OK req '() mime:json
       (jsexpr->string (query (bytes->jsexpr (request-post-data/raw req))))))
+(define (/v2/index.js  req) (OK req '() mime:js   v2:index.js))
+(define (/v2/query     req)
+  (OK req '() mime:json
+      (jsexpr->string (query (bytes->jsexpr (request-post-data/raw req))))))
 
 (define (start)
   (define-values (dispatch _)
     (dispatch-rules
-      (("")             #:method         "get"  index)
-      (("index.js")     #:method         "get"  /index.js)
-      (("schema.json")  #:method         "get"  /schema.json)
-      (("schema.yaml")  #:method         "get"  /schema.yaml)
-      (("schema.html")  #:method         "get"  /schema.html)
-      (("schema.html2") #:method         "get"  /schema.html2)
-      (("predicates")   #:method         "get"  /predicates)
-      (("query")        #:method         "post" /query)
-      (else                                     not-found)))
+      (("")              #:method  "get"  /index)
+      (("index.js")      #:method  "get"  /index.js)
+      (("schema.json")   #:method  "get"  /schema.json)
+      (("schema.yaml")   #:method  "get"  /schema.yaml)
+      (("schema.html")   #:method  "get"  /schema.html)
+      (("schema.html2")  #:method  "get"  /schema.html2)
+      (("predicates")    #:method  "get"  /predicates)
+      (("query")         #:method  "post" /query)
+      (("v2")            #:method  "get"  /v2/index)
+      (("v2" "index.js") #:method  "get"  /v2/index.js)
+      (("v2" "query")    #:method  "post" /v2/query)
+      (else                               not-found)))
   (serve/servlet dispatch
                  ;; none-manager for better performance:
                  ;; only possible because we're not using web continuations.
