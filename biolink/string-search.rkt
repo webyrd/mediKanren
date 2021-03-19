11bb5b89a0759b880eadab8f1f85fa4f5588bb7a
gregr <greg.weiqi@gmail.com>
2021-03-18 22:15:00 -0400

    Define string:corpus-find*/disk

--- biolink/string-search.rkt
+++ biolink/string-search.rkt
@@ -4,7 +4,8 @@
   suffix:corpus->index
   suffix:corpus-find*
   string:corpus->index
-  string:corpus-find*)
+  string:corpus-find*
+  string:corpus-find*/disk)
 (require
   "repr.rkt"
   racket/list
@@ -239,4 +240,46 @@
 (define (string:corpus-find* corpus index str*)
   (remove-duplicates
     (sort (append* (map (lambda (s) (string:corpus-find corpus index s)) str*))
+          <)))
+
+(define (string:corpus-find/disk cid->concept in-index needle)
+  (define (compare si needle)
+    (define cid (string-index->string-key in-index si))
+    (define hay (concept-cui (cid->concept cid)))
+    (cond ((string=? hay needle)  0)
+          ((string<? hay needle) -1)
+          (else                   1)))
+  ;; Find a point in the desired range...
+  (let find-range ((start 0) (end (string-key-count in-index)))
+    (cond ((< start end)
+           (define mid (+ start (quotient (- end start) 2)))
+           (case (compare mid needle)
+             ((-1) (find-range (+ 1 mid) end))
+             (( 1) (find-range start mid))
+             (( 0) ;; ... then find the start and end of that range.
+              (define rstart
+                (let loop ((start start) (end mid))
+                  (cond ((< start end)
+                         (define mid (+ start (quotient (- end start) 2)))
+                         (case (compare mid needle)
+                           ((-1) (loop (+ 1 mid) end))
+                           (( 0) (loop start mid))
+                           (else (error "rstart: this shouldn't happen."))))
+                        (else end))))
+              (define rend
+                (let loop ((start (+ 1 mid)) (end end))
+                  (cond ((< start end)
+                         (define mid (+ start (quotient (- end start) 2)))
+                         (case (compare mid needle)
+                           ((1) (loop start mid))
+                           ((0) (loop (+ 1 mid) end))
+                           (else (error "rend: this shouldn't happen."))))
+                        (else end))))
+              (remove-duplicates (map (lambda (i) (string-index->string-key in-index i))
+                                      (range rstart rend))))))
+          (else '()))))
+
+(define (string:corpus-find*/disk cid->concept in-index str*)
+  (remove-duplicates
+    (sort (append* (map (lambda (s) (string:corpus-find/disk cid->concept in-index s)) str*))
           <)))
