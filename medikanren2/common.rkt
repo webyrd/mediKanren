5c43a2cfebaae03a9026e6f4ed9c88f9f48e4169
gregr <greg.weiqi@gmail.com>
2021-03-23 15:47:35 -0400

    Describe the intention of common.rkt

--- medikanren2/common.rkt
+++ medikanren2/common.rkt
@@ -1,3 +1,11 @@
 #lang racket/base
-(provide (all-from-out "base.rkt"))
-(require "base.rkt")
+(provide (all-from-out "base.rkt"
+                       "db/semmed.rkt"
+                       "db/rtx2-20210204.rkt"))
+(require "base.rkt"
+         (prefix-in semmed: "db/semmed.rkt")
+         (prefix-in rtx:    "db/rtx2-20210204.rkt"))
+
+;; TODO: load databases dynamically based on config
+
+;; TODO: define higher-level relations over the db-specific relations
