a165e99ae6d6418bde81c773b33ef6d4405529db
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-03-25 21:35:52 -0700

    fix: path
    amend

--- medikanren/pieces-parts/Thi-useful-functions.rkt
+++ medikanren/pieces-parts/Thi-useful-functions.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (provide (all-defined-out))
-(require "../../../medikanren/pieces-parts/query.rkt")
+(require "query.rkt")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;USEFUL FUNCTIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
