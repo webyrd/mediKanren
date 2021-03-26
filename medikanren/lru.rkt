894aa810a147cf168c9d0722dac3e68e23f373c4
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-03-26 16:38:13 -0700

    implement: lru.rkt
    refactor: extract struct payload
    
    implement: unit test demonstrating lru bug
    
    fix: lru-freshen: the case of oldest lrun and newest lrun
    
    fix: handling an empty linked list would be more work, and useless, so skip it
    
    refactor: extract define lru-remove
    
    refactor: move all lru-evict calls to lru-ref
    
    amend: refactor: move all lru-evict calls to lru-ref
    
    refactor: use lru-remove from lru-evict
    
    refactor: remove unnecessary lru-evict call
    
    refactor: make all the counting work the same way
    
    renames/comments/formatting

---
+++ medikanren/lru.rkt
@@ -0,0 +1,234 @@
+#lang racket
+(provide
+ make-lru
+ lru-ref
+ )
+(require chk)
+
+
+(define (assert k st)
+  (if (not k)
+      (raise (format "assertion failure: ~a" st))
+      #f))
+
+(struct payload (k v))
+
+(struct lrun (
+              (older #:mutable)
+              payload
+              (newer #:mutable)))
+
+(struct lru (
+             behind-ref
+             hash
+             (lrun-oldest #:mutable)
+             (lrun-newest #:mutable)
+             (num-entries #:mutable)
+             num-entries-max))
+
+(define (make-lru behind-ref #:num-entries-max (num-entries-max 1000))
+  (lru
+   behind-ref
+   (make-hash)
+   #f
+   #f
+   0
+   num-entries-max))
+
+;;; Add a newest entry to the lru.
+(define (lru-put-newest ths payload1)
+  (let* (
+         (lrun1 (lru-lrun-newest ths))
+         (k (payload-k payload1))
+         (lrun0 (lrun lrun1 payload1 #f))
+         )
+    ; connect lrun fields
+    ; lrun0.older is already connected
+    (set-lrun-older! lrun0 lrun1) 
+    (if lrun1
+        (set-lrun-newer! lrun1 lrun0)
+        #f)
+    ; connect lru fields
+    (set-lru-lrun-newest! ths lrun0)
+    (if (not (lru-lrun-oldest ths)) ; are we brand new?
+        (set-lru-lrun-oldest! ths lrun0)
+        #f)
+    (set-lru-num-entries! ths (+ (lru-num-entries ths) 1))
+    (hash-set! (lru-hash ths) k lrun0)))
+
+(define (lru-remove ths lrun1)
+  (let* (
+         (payload1 (lrun-payload lrun1))
+         (k (payload-k payload1))
+         (lrun0 (lrun-older lrun1))
+         (lrun2 (lrun-newer lrun1))
+         )
+    (if lrun0
+        (set-lrun-newer! lrun0 lrun2)
+        (begin
+          ; we are removing the oldest
+          (set-lru-lrun-oldest! ths lrun2)
+          (set-lrun-older! lrun2 #f)))
+    (if lrun2
+        (set-lrun-older! lrun2 lrun0)
+        (begin
+          ; we are removing the newest
+          (set-lru-lrun-newest! ths lrun0) 
+          (set-lrun-newer! lrun0 #f)))
+    (set-lru-num-entries! ths (- (lru-num-entries ths) 1))
+    (hash-remove! (lru-hash ths) k)))
+
+
+;;; If the lru is full, remove the oldest entry.
+(define (lru-evict ths)
+  (if (> (lru-num-entries ths) (lru-num-entries-max ths))
+      (let* ((lrun1 (lru-lrun-oldest ths)))
+        (lru-remove ths lrun1))
+      #f))
+
+;;; Make the entry with key k the newest entry.
+(define (lru-freshen ths k)
+  (let* (
+         (lrun1 (hash-ref (lru-hash ths) k))
+         (payload1 (lrun-payload lrun1))
+          )
+    (lru-remove ths lrun1)
+    (lru-put-newest ths payload1)))
+
+
+;;; Fetch item from lru cache, or if absent, from ref-behind.
+(define (lru-ref ths k)
+  (match (hash-ref! (lru-hash ths) k #f)
+    (#f
+     (let ((v ((lru-behind-ref ths) k)))
+       (lru-put-newest ths (payload k v))
+       (lru-evict ths)
+       v))
+    (lrun
+     (if (>= (lru-num-entries ths) 2) ; freshen 1 entry is noop
+         (lru-freshen ths k)
+         #f)
+     (payload-v (lrun-payload lrun)))))
+
+
+
+(module+ test
+  (define (make-test-hash n)
+    (define h (make-hash))
+    (for ((i (range n)))
+      (hash-set! h i (+ i 100)))
+    h)
+
+  ;; do we get a correct value back on a miss?
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define l (make-lru (lambda (k) (hash-ref h1 k #f)) #:num-entries-max 3))
+   #:do (define v (lru-ref l 5))
+   #:= v 105)
+
+  ;; do we get a correct value back on a hit?
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define l (make-lru (lambda (k) (hash-ref h1 k #f)) #:num-entries-max 3))
+   #:do (lru-ref l 5)
+   #:do (define v (lru-ref l 5))
+   #:= v 105)
+
+  ;; do we initialize properly?
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define l (make-lru (lambda (k) (hash-ref h1 k #f)) #:num-entries-max 3))
+   #:do (define v (lru-ref l 5))
+   #:t (lru-lrun-newest l)
+   #:t (lru-lrun-oldest l))
+
+  ;; when the oldest entry becomes the newest entry, is the next eviction correct?
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define l (make-lru (lambda (k) (hash-ref h1 k #f)) #:num-entries-max 3))
+   #:do (lru-ref l 5)
+   #:do (lru-ref l 6)
+   #:do (lru-ref l 7)
+   #:do (lru-ref l 5)
+   #:do (lru-ref l 8)
+   #:t (not (hash-has-key? (lru-hash l) 6)))
+
+  ;; does num-entries grow as expeced?
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define l (make-lru (lambda (k) (hash-ref h1 k #f)) #:num-entries-max 3))
+   #:do (lru-ref l 5)
+   #:= (lru-num-entries l) 1)
+
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define l (make-lru (lambda (k) (hash-ref h1 k #f)) #:num-entries-max 3))
+   #:do (lru-ref l 5)
+   #:do (lru-ref l 6)
+   #:= (lru-num-entries l) 2)
+
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define l (make-lru (lambda (k) (hash-ref h1 k #f)) #:num-entries-max 3))
+   #:do (lru-ref l 5)
+   #:do (lru-ref l 6)
+   #:do (lru-ref l 7)
+   #:= (lru-num-entries l) 3)
+
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define l (make-lru (lambda (k) (hash-ref h1 k #f)) #:num-entries-max 3))
+   #:do (lru-ref l 5)
+   #:do (lru-ref l 6)
+   #:do (lru-ref l 7)
+   #:do (lru-ref l 8)
+   #:= (lru-num-entries l) 3)
+
+  ;; do we make the expected number of upstream calls?
+
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define num-calls (box 0))
+   #:do (define (incr) (set-box! num-calls (+ 1 (unbox num-calls))))
+   #:do (define l (make-lru
+                   (lambda (k) (incr) (hash-ref h1 k #f))
+                   #:num-entries-max 2))
+   #:do (lru-ref l 5)
+   #:= (unbox num-calls) 1)
+  
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define num-calls (box 0))
+   #:do (define (incr) (set-box! num-calls (+ 1 (unbox num-calls))))
+   #:do (define l (make-lru
+                   (lambda (k) (incr) (hash-ref h1 k #f))
+                   #:num-entries-max 2))
+   #:do (lru-ref l 5)
+   #:do (lru-ref l 5)
+   #:= (unbox num-calls) 1)
+  
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define num-calls (box 0))
+   #:do (define (incr) (set-box! num-calls (+ 1 (unbox num-calls))))
+   #:do (define l (make-lru
+                   (lambda (k) (incr) (hash-ref h1 k #f))
+                   #:num-entries-max 2))
+   #:do (lru-ref l 5)
+   #:do (lru-ref l 6)
+   #:do (lru-ref l 5)
+   #:= (unbox num-calls) 2)
+
+  (chk
+   #:do (define h1 (make-test-hash 100))
+   #:do (define num-calls (box 0))
+   #:do (define (incr) (set-box! num-calls (+ 1 (unbox num-calls))))
+   #:do (define l (make-lru
+                   (lambda (k) (incr) (hash-ref h1 k #f))
+                   #:num-entries-max 2))
+   #:do (lru-ref l 5)
+   #:do (lru-ref l 6)
+   #:do (lru-ref l 7)
+   #:do (lru-ref l 5)
+   #:= (unbox num-calls) 4)
+  )
