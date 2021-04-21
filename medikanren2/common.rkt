8848234814b5cb4afe5863646c49f202be2420e9
Nathaniel Rudavsky-Brody <nathaniel.rudavsky@posteo.org>
2021-04-21 15:03:09 -0700

    Extend semantic-web relations over multiple databases

--- medikanren2/common.rkt
+++ medikanren2/common.rkt
@@ -2,7 +2,9 @@
 (provide (all-from-out "base.rkt"
                        "db/semmed.rkt"
                        "db/rtx2-20210204.rkt")
-         cprop edge eprop triple is-a triple-property)
+         cprop edge eprop
+         triple quad triple/eid is-a is-a/quad triple-property)
+
 (require "base.rkt"
          (prefix-in semmed: "db/semmed.rkt")
          (prefix-in rtx:    "db/rtx2-20210204.rkt")
@@ -49,25 +51,57 @@
 
 ;; Semantic-web flavored relations
 
-(define-relation (quad graph s p o)
-  (fresh (id eid)
-    (== eid `(,graph . ,id))
-    (eprop eid "predicate" p)
-    (edge eid s o)))
+(define-relation (rtx:triple s p o)
+    (fresh (id)
+      (rtx:eprop id "predicate" p)
+      (rtx:edge id s o)))
+
+(define-relation (semmed:triple s p o)
+    (fresh (id)
+      (semmed:eprop id "edge_label" p)
+      (semmed:edge id s o)))
 
 (define-relation (triple s p o)
-  (fresh (eid)
-    (eprop eid "predicate" p)
-    (edge eid s o)))
+  (conde ((rtx:triple s p o))
+         ((semmed:triple s p o))))
+
+(define-relation (quad graph s p o)
+  (fresh (id)
+    (conde ((== graph 'rtx2-20210204)
+            (rtx:triple s p o))
+           ((== graph 'semmed)
+            (semmed:triple s p o)))))
+
+(define-relation (triple/eid eid s p o)
+  (fresh (id graph)
+    (== eid `(,graph . ,id))
+    (conde ((== graph 'rtx2-20210204)
+            (rtx:eprop id "predicate" p)
+            (rtx:edge id s o))
+           ((== graph 'semmed)
+            (semmed:eprop id "edge_label" p)
+            (semmed:edge id s o)))))
 
 (define-relation (is-a s c)
   (cprop s "category" c))
 
+(define-relation (is-a/quad graph s c)
+  (conde ((== graph 'rtx2-20210204)
+          (rtx:cprop s "category" c))
+         ((== graph 'semmed)
+          (semmed:cprop s "category" c))))
+
 (define-relation (triple-property s p o k v)
-  (fresh (eid)
-    (edge eid s o)
-    (eprop eid "predicate" p)
-    (eprop eid k v)))
+  (fresh (eid id graph)
+    (== eid `(,graph . ,id))
+    (conde ((== graph 'rtx2-20210204)
+            (rtx:eprop id "predicate" p)
+            (rtx:edge id s o)
+            (rtx:eprop id k v))
+           ((== graph 'semmed)
+            (semmed:eprop id "edge_label" p)
+            (semmed:edge id s o)
+            (semmed:eprop id k v)))))
 
 (define-relation (edge-predicate eid p)
   (eprop eid "predicate" p))
