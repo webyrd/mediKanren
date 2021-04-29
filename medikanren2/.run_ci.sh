1cb3cc83e179cbcf8fe766a4d8f78e4d93e3ad67
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-04-28 17:11:11 -0700

    implement: medikanren 2 unit test against real data on github actions

--- medikanren2/.run_ci.sh
+++ medikanren2/.run_ci.sh
@@ -3,5 +3,14 @@ adirRepo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 adirMk="$adirRepo/medikanren2"
 adirArtifacts="$adirRepo/ci_artifacts"
 
-echo medikanren2_run_ci > "$adirArtifacts/status/pass/medikanren2_run_ci"
+# TODO: Replace this way to run tests with a way that actually
+# discovers them.  The purpose of the current way only to show that
+# tests can be run in Github Actions against real data.
+
+if raco test "$adirMk/test/10GB-CI/with-yeast-sri-reference-kg.rkt" 2>&1 | tee "$adirRepo/ci_artifacts/raco_test.log"
+then
+    echo medikanren_run_ci > "$adirArtifacts/status/pass/medikanren2_run_ci"
+else
+    echo medikanren_run_ci > "$adirArtifacts/status/fail/medikanren2_run_ci"
+fi
 
