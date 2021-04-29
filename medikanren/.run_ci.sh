653f37bd18cd28c8947992a1d6ab2d39c68dadec
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-04-28 18:37:50 -0700

    fix: tee overwriting error status

--- medikanren/.run_ci.sh
+++ medikanren/.run_ci.sh
@@ -3,7 +3,7 @@ adirRepo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 adirMk="$adirRepo/medikanren"
 adirArtifacts="$adirRepo/ci_artifacts"
 
-if raco test "$adirMk/configref.rkt" 2>&1 | tee "$adirRepo/ci_artifacts/raco_test.log"
+if raco test "$adirMk/configref.rkt"
 then
     echo medikanren_run_ci > "$adirArtifacts/status/pass/medikanren1_run_ci"
 else
