daf35ad9a402c555ce3ee4061d10c8d206ca7cba
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-04-29 10:21:29 -0700

    CI: run against yeast-sri-reference-kg test fixture
    Squashed commit of the following:
    
    commit 4c4cef8174bfa7f9d6d7ea88875734486563bba5
    Author: Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
    Date:   Thu Apr 29 10:21:15 2021 -0700
    
        comment
    
    commit 5cff03521d6b155366af5375095cae06fc107e4f
    Author: Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
    Date:   Wed Apr 28 18:44:31 2021 -0700
    
        amend: implement: medikanren 2 unit test against real data on github actions
    
    commit 653f37bd18cd28c8947992a1d6ab2d39c68dadec
    Author: Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
    Date:   Wed Apr 28 18:37:50 2021 -0700
    
        fix: tee overwriting error status
    
    commit 4ed33018339fe2330fbd081a4ef9d9ac6c07fe1e
    Author: Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
    Date:   Wed Apr 28 18:34:03 2021 -0700
    
        troubleshooting: tee overwriting error status?
    
    commit aa916f60f3ba407a396bcfe6986e261779ca5b17
    Author: Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
    Date:   Wed Apr 28 18:31:37 2021 -0700
    
        amend
    
    commit 1cb3cc83e179cbcf8fe766a4d8f78e4d93e3ad67
    Author: Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
    Date:   Wed Apr 28 17:11:11 2021 -0700
    
        implement: medikanren 2 unit test against real data on github actions

--- medikanren/.run_ci.sh
+++ medikanren/.run_ci.sh
@@ -3,7 +3,11 @@ adirRepo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 adirMk="$adirRepo/medikanren"
 adirArtifacts="$adirRepo/ci_artifacts"
 
-if raco test "$adirMk/configref.rkt" 2>&1 | tee "$adirRepo/ci_artifacts/raco_test.log"
+# TODO: Replace this way to run tests with a way that actually
+# discovers them.  The purpose of the current way only to show that
+# tests can be run in Github Actions against real data.
+
+if raco test "$adirMk/configref.rkt"
 then
     echo medikanren_run_ci > "$adirArtifacts/status/pass/medikanren1_run_ci"
 else
---
+++ medikanren2/test/10GB-CI/with-yeast-sri-reference-kg.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(require chk)
+(require "../../db/yeast-sri-reference-kg-0.3.0.rkt")
+(require "../../dbk/mk.rkt")
+
+
+(define uri-scn4a "ENSEMBL:ENSBTAG00000004770")
+
+(chk
+ (#:= (run* (p o)
+            (fresh (id)
+                   (== id uri-scn4a)
+                   (membero p '("in_taxon" "description"))
+                   (nodes id p o)))
+  '(("in_taxon" "NCBITaxon:9913|NCBITaxon:9913")
+    ("description" "Sodium channel protein"))))
+
+
+(define uri-ion-transport "GO:0034765")
+
+(chk
+ (#:= (run* (p o)
+            (fresh (id)
+                   (== id uri-ion-transport)
+                   (membero p '("iri" "name"))
+                   (nodes id p o)))
+  '(("iri" "http://purl.obolibrary.org/obo/GO_0034765")
+    ("name" "regulation of ion transmembrane transport"))))
+
+
