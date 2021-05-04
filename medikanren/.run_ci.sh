4597212696540f32ed90accf2f3c9253d59df3ad
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-05-04 10:20:58 -0700

    CI: precompile apps

--- medikanren/.run_ci.sh
+++ medikanren/.run_ci.sh
@@ -3,6 +3,23 @@ adirRepo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 adirMk="$adirRepo/medikanren"
 adirArtifacts="$adirRepo/ci_artifacts"
 
+stepname=medikanren1_compile_trapi
+if raco make "$adirMk/apps/translator-web-server.rkt"
+then
+    echo "$stepname" > "$adirArtifacts/status/pass/$stepname"
+else
+    echo "$stepname" > "$adirArtifacts/status/fail/$stepname"
+fi
+
+stepname=medikanren1_compile_gui
+if raco make "$adirMk/apps/gui-simple-v2.rkt"
+then
+    echo "$stepname" > "$adirArtifacts/status/pass/$stepname"
+else
+    echo "$stepname" > "$adirArtifacts/status/fail/$stepname"
+fi
+
+
 # TODO: Replace this way to run tests with a way that actually
 # discovers them.  The purpose of the current way only to show that
 # tests can be run in Github Actions against real data.
@@ -13,7 +30,5 @@ then
 else
     echo medikanren_run_ci > "$adirArtifacts/status/fail/medikanren1_run_ci"
 fi
-
-
 
 
