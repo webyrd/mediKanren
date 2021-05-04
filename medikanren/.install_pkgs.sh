6390aefafe1cdfb8771f93125cc8127d4972209b
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-05-04 13:19:01 -0700

    CI: implement: move package declarations to mediKanren repository

---
+++ medikanren/.install_pkgs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO: raco pkg will stop and do nothing if you run
+# this command as-is and any of the packages have been previously installed.
+
+# Current workaround is to only install it once, from the medikanren2 directory.
+
+# raco pkg install --auto --batch --fail-fast \
+#     chk \
+#     shell-pipeline \
+#     yaml \
+#     memoize
