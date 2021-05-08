24f8013b3948488461b1608e4ae4c63e64722d16
Nathaniel Rudavsky-Brody <nathaniel.rudavsky@posteo.org>
2021-05-07 17:35:45 -0700

    Clarify syntax of lightweight reasoning relations

--- medikanren2/lw-reasoning.rkt
+++ medikanren2/lw-reasoning.rkt
@@ -96,35 +96,35 @@
   (conde ((== child parent))
          ((subclass-of+ child parent))))
 
-(define-relation (edge-predicate/subclass eid p p^)
+(define-relation (edge-predicate/subclass eid p^ p)
   (conde ((eprop eid "predicate" p))
          ((subclass-of+ p^ p)
           (eprop eid "predicate" p^))))
 
-(define-relation (is-a/subclass* s c c^)
+(define-relation (is-a/subclass* s c^ c)
   (is-a s c^)
   (subclass-of* c^ c))
 
-(define-relation (is-a/subclass+ s c c^)
+(define-relation (is-a/subclass+ s c^ c)
   (subclass-of+ c^ c)
   (is-a s c^))
 
 ;; for testing only; not a useful relation as far as I can see
-(define-relation (triple/subclass+ s p p^ o)
+(define-relation (triple/subclass+ s p^ p o)
   (subclass-of+ p^ p)
   (triple/inverse s p^ o)) 
 
-(define-relation (triple/subclass s p p^ o)
+(define-relation (triple/subclass s p^ p o)
   (conde ((triple s p o) (== p p^))
          ((triple/subclass+ s p p^ o))))
 
-(define-relation (triple/reasoning s s^ p p^ o o^)
+(define-relation (triple/reasoning s^ s p^ p o^ o)
   (subclass-of* s^ s)
   (subclass-of* p^ p)
   (subclass-of* o^ o)
   (triple s^ p^ o^))
 
-(define-relation (triple/reasoning+ s s^ p p^ o o^)
+(define-relation (triple/reasoning+ s^ s p^ p o^ o)
   (triple s^ p^ o^)
   (subclass-of+ s^ s)
   (subclass-of+ p^ p)
