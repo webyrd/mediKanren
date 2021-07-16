7729dc890f20ee64086e2f283ece71766f978816
gregr <greg.weiqi@gmail.com>
2021-07-16 10:47:06 -0400

    Remove redundant node

--- medikanren2/synonyms.rkt
+++ medikanren2/synonyms.rkt
@@ -26,7 +26,7 @@
       (conde ((==       x b))
              ((equiv=/= x b)))))
   (define-relation (equiv a b)
-    (conde ((==       a b) (node a) (node b))
+    (conde ((==       a b) (node a))
            ((equiv=/= a b))))
   equiv)
 
