c6e6343dafffcf9cf351d74d3ed1642fdfd9d94d
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-07-21 15:00:56 -0700

    pin raco pkgs: implement: pin versions of raco packages

---
+++ pkgs/mediKanren-dependencies/info.rkt
@@ -0,0 +1,13 @@
+#lang info
+(define collection "mediKanren-dependencies")
+(define deps '(
+               "chk"
+               "shell-pipeline"
+               "yaml"
+               "memoize"
+               ))
+(define build-deps '())
+(define scribblings '())
+(define pkg-desc "Dummy package to install all mediKanren dependencies")
+(define version "0.0")
+(define pkg-authors '())
