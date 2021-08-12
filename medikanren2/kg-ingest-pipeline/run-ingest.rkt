b8035f559ad53df4a99f558c26333899345ae66e
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-08-12 10:43:33 -0700

    refactor: run-ingest.rkt: extract with-context

--- medikanren2/kg-ingest-pipeline/run-ingest.rkt
+++ medikanren2/kg-ingest-pipeline/run-ingest.rkt
@@ -6,22 +6,26 @@
 
 (define (fetch-recent-tasks) '()) ; TODO: in task-queue.rkt
 
-(define (main)
+(define (with-context thunk)
   (with-config
     (lambda ()
       (with-adir-temp-root
         (lambda ()
           (parameterize ((dry-run #f))
             (with-kge-token
-              (lambda ()
-                (let* (
-                       (idvers (log-thunk (lambda () (fetch-recent-kge-versions)) 'fetch-recent-kge-versions))
-                       (kgmetas (log-thunk (lambda () (fetch-recent-kgmeta idvers)) 'fetch-recent-kgmeta))
-                       (tasks (fetch-recent-tasks))
-                       (tbis (log-thunk (lambda () (tbis-tosync kgmetas tasks)) 'tbis-tosync kgmetas tasks)))
-                  (for ((tbi tbis))
-                    (fetch-payload-to-disk tbi)
-                    (process-tbi (s3path-base) tbi)))))))))))
+              thunk)))))))
+
+(define (main)
+  (with-context
+    (lambda ()
+      (let* (
+             (idvers (log-thunk (lambda () (fetch-recent-kge-versions)) 'fetch-recent-kge-versions))
+             (kgmetas (log-thunk (lambda () (fetch-recent-kgmeta idvers)) 'fetch-recent-kgmeta))
+             (tasks (fetch-recent-tasks))
+             (tbis (log-thunk (lambda () (tbis-tosync kgmetas tasks)) 'tbis-tosync kgmetas tasks)))
+        (for ((tbi tbis))
+          (fetch-payload-to-disk tbi)
+          (process-tbi (s3path-base) tbi))))))
 
 (module+ main
   (main))
