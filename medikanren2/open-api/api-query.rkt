dc680174777978f3047347dc91154ab2ca74db35
William Byrd <webyrd@gmail.com>
2021-08-12 10:54:14 -0400

    manually reverted api-query.rkt changes

--- medikanren2/open-api/api-query.rkt
+++ medikanren2/open-api/api-query.rkt
@@ -30,31 +30,21 @@
   "/query")
 
 (define (api-query url-string (optional-post-jsexpr (void)))
-  (printf "##### making api-query call to url:\n~s\n" url-string) 
   (define-values (status headers in)
     (time (if (void? optional-post-jsexpr)
-              (http-sendrecv/url
-               (string->url url-string)
-               #:method "GET")
-              (http-sendrecv/url
-               (string->url url-string)
-               #:method "POST"
-               #:data (jsexpr->string optional-post-jsexpr)
-               #:headers '("Content-Type: application/json; charset=utf-8")))))
+            (http-sendrecv/url
+              (string->url url-string)
+              #:method "GET")
+            (http-sendrecv/url
+              (string->url url-string)
+              #:method "POST"
+              #:data (jsexpr->string optional-post-jsexpr)
+              #:headers '("Content-Type: application/json; charset=utf-8")))))
   (define response-string (time (port->string in)))
-  (printf "status:\n~s\n" status)
-  (define OK-status? (bytes=? #"HTTP/1.1 200 OK" status))
-  (define bad-gateway-status? (bytes=? #"HTTP/1.1 502 Bad Gateway" status))
-  (if bad-gateway-status?
-      (begin
-        (printf "!!!!!! unexpected non-OK status returned from ~s:\n" url-string)
-        (pretty-print `(,status ,headers ,response-string))
-        (hash 'status status
-              'headers headers
-              'response (hash)))
-      (hash 'status status
-            'headers headers
-            'response (string->jsexpr response-string))))
+ ;(pretty-print `(,status ,headers ,response-string))
+  (hash 'status status
+        'headers headers
+        'response (string->jsexpr response-string)))
 
 (define (js-query edges nodes)
   (hash 'message
