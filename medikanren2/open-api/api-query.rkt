70c4a73296fd0de00a84b50e6a98d31e2ca5e219
William Byrd <webyrd@gmail.com>
2021-08-12 10:27:26 -0400

    added graceful handling of non-200 HTTP responses to api-query.rkt

--- medikanren2/open-api/api-query.rkt
+++ medikanren2/open-api/api-query.rkt
@@ -30,21 +30,30 @@
   "/query")
 
 (define (api-query url-string (optional-post-jsexpr (void)))
+  (printf "##### making api-query call to url:\n~s\n" url-string) 
   (define-values (status headers in)
     (time (if (void? optional-post-jsexpr)
-            (http-sendrecv/url
-              (string->url url-string)
-              #:method "GET")
-            (http-sendrecv/url
-              (string->url url-string)
-              #:method "POST"
-              #:data (jsexpr->string optional-post-jsexpr)
-              #:headers '("Content-Type: application/json; charset=utf-8")))))
+              (http-sendrecv/url
+               (string->url url-string)
+               #:method "GET")
+              (http-sendrecv/url
+               (string->url url-string)
+               #:method "POST"
+               #:data (jsexpr->string optional-post-jsexpr)
+               #:headers '("Content-Type: application/json; charset=utf-8")))))
   (define response-string (time (port->string in)))
-  ;(pretty-print `(,status ,headers ,response-string))
-  (hash 'status status
-        'headers headers
-        'response (string->jsexpr response-string)))
+  (printf "status:\n~s\n" status)
+  (define OK-status? (bytes=? #"HTTP/1.1 200 OK" status))
+  (if OK-status?
+      (hash 'status status
+            'headers headers
+            'response (string->jsexpr response-string))
+      (begin
+        (printf "!!!!!! unexpected non-OK status returned from ~s:\n" url-string)
+        (pretty-print `(,status ,headers ,response-string))
+        (hash 'status status
+              'headers headers
+              'response (hash)))))
 
 (define (js-query edges nodes)
   (hash 'message
