9a2748ebb54755fe2a36c56296ac188472c2d22f
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-08-26 11:34:37 -0700

    implement: yaml-json-compat

---
+++ medikanren2/kg-ingest-pipeline/yaml-json-compat.rkt
@@ -0,0 +1,63 @@
+#lang racket
+(provide
+    sjsexpr->string
+    sjsexpr->bytes)
+(require json)
+(require chk)
+
+#|
+    Despite yaml being an isomorphism of json, the racket yaml and json packages bizarrely do not
+    agree on whether strings (yaml) or symbols (json) are to be used as the representation for keys
+    in racket.
+
+    Escape hatch, so long as you aren't concerned about the performance side of the problem.
+|#
+
+(define (keynorm-as-json k)
+    (cond
+        ((string? k) (string->symbol k))
+        ((symbol? k) k)
+        (else k)))
+
+(define (dictkeynorm keynorm expr)
+  (define (walk-kvs kvs kvs-done)
+    (if (null? kvs)
+        (make-hash (reverse kvs-done))
+        (let* ((kv (car kvs))
+               (k (keynorm (car kv)))
+               (v (walk (cdr kv))))
+          (walk-kvs (cdr kvs) (cons (cons k v) kvs-done)))))
+  (define (walk-list expr expr-done)
+    (if (null? expr)
+        (reverse expr-done)
+        (walk-list (cdr expr) (cons (walk (car expr)) expr-done))))
+  (define (walk expr)
+    (cond 
+      ((dict? expr)
+       (let ((kvs (dict->list expr)))
+         (walk-kvs kvs '())))
+      ((list? expr)
+       (walk-list expr '()))
+      (else expr)))
+  (walk expr))
+
+(define (sjsexpr->string expr)
+  (jsexpr->string (dictkeynorm keynorm-as-json expr)))
+
+(define (sjsexpr->bytes expr)
+  (jsexpr->bytes (dictkeynorm keynorm-as-json expr)))
+
+(module+ test
+  (chk
+    (#:=
+      (sjsexpr->string `#hash(("hello" . "world")))
+      "{\"hello\":\"world\"}"))
+  (chk
+    (#:=
+      (sjsexpr->string `("hello" "world"))
+      "[\"hello\",\"world\"]"))
+  (chk
+    (#:t (symbol? (car (dict-keys (car (dictkeynorm keynorm-as-json `(#hash(("hello" . "world"))))))))))
+  (chk
+    (#:t (symbol? (car (dict-keys (car (dictkeynorm keynorm-as-json `(#hash(("hello" . #hash(("hello" . "world"))))))))))))
+)
