017fb5d0ca32aa6b3fdda8555a13394eb267dccf
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-08-27 16:36:38 -0700

    implement: prepare to enable errortrace

--- medikanren2/kg-ingest-pipeline/write-require-racket.sh
+++ medikanren2/kg-ingest-pipeline/write-require-racket.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-afileRkt="$1"
+rfileRkt="$1"
 afileOut="$2"
 
 rm -f "$afileOut"
 printf "#lang racket\n" >> "$afileOut"
-printf "(require \"%s\")\n" "$afileRkt" >> "$afileOut"
+printf "(require \"%s\")\n" "$rfileRkt" >> "$afileOut"
