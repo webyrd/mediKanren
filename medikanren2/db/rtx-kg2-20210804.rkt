6028e046bc761141c3a3231dcc79494a1c5289e8
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-08-31 11:38:51 -0700

    rename rtx-kg2 date 20210816 => 20210804

--- medikanren2/db/rtx-kg2-20210804.rkt
+++ medikanren2/db/rtx-kg2-20210804.rkt
@@ -9,7 +9,7 @@
     (cprop curie k v)))
 
 (define-relation/table cprop
-  'path               "rtx-kg2/20210816/cprop"
+  'path               "rtx-kg2/20210804/cprop"
   'source-file-path   "upstream/rtx-kg2/rtx_kg2.nodeprop.tsv"
   'source-file-header '(:ID propname value)
   'attribute-names    '(curie key value)
@@ -20,7 +20,7 @@
 (string-search-init-rel cprop)
 
 (define-relation/table edge
-  'path               "rtx-kg2/20210816/edge"
+  'path               "rtx-kg2/20210804/edge"
   'source-file-path   "upstream/rtx-kg2/rtx_kg2.edge.tsv"
   'source-file-header '(":ID" ":START" ":END")
   'map                (value/syntax
@@ -33,7 +33,7 @@
                         (object subject)))
 
 (define-relation/table eprop
-  'path               "rtx-kg2/20210816/eprop"
+  'path               "rtx-kg2/20210804/eprop"
   'source-file-path   "upstream/rtx-kg2/rtx_kg2.edgeprop.tsv"
   'source-file-header '(":ID" "propname" "value")
   'map                (value/syntax
