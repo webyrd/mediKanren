3abac994015201f177418de4e77f35cd5c9866a9
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-08-30 21:21:33 -0700

    config: move browserbot_kge_client to be a direct sibling of mediKanren

--- medikanren2/kg-ingest-pipeline/kge-params.rkt
+++ medikanren2/kg-ingest-pipeline/kge-params.rkt
@@ -9,7 +9,7 @@
 
 (define (with-kge-token thunk-run)
   (define afile-token 
-    (simplify-path (build-path (adir-current-source) 'up 'up 'up "kge" "browserbot_kge_client" "session_token.json")))
+    (simplify-path (build-path (adir-current-source) 'up 'up 'up "browserbot_kge_client" "session_token.json")))
   (define st-token (file->string afile-token))
   (define jsexpr-token (dict-ref (string->jsexpr st-token) 'value))
   (parameterize ((kge-token jsexpr-token))
