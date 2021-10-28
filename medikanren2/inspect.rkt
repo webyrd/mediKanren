a9cabdb002360eb1f7076094bd648a720167da34
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-10-28 12:56:45 -0700

    inspect.rkt: logging: add subquery level to run, run*, run*/set

--- medikanren2/inspect.rkt
+++ medikanren2/inspect.rkt
@@ -2,28 +2,35 @@
 (require "dbk.rkt")
 (provide runi runi* runi*/set)
 
+;; When a subquery runs, increment this number.
+(define i-level (make-parameter 0))
+;; TODO: instrumenting levels on run^ will be harder
+
 (define-syntax (runi stx)
     (syntax-case stx ()
         [(_ n formals expr)
             #'(begin
-                (pretty-write (list 'run n (query formals expr)))
-                (run n formals expr))]
+                (pretty-write (list 'run n (list 'level (i-level)) (query formals expr)))
+                (parameterize ((i-level (+ 1 (i-level))))
+                    (run n formals expr)))]
     ))
 
 (define-syntax (runi* stx)
     (syntax-case stx ()
         [(_ formals expr)
             #'(begin
-                (pretty-write (list 'run* (query formals expr)))
-                (run* formals expr))]
+                (pretty-write (list 'run* (list 'level (i-level)) (query formals expr)))
+                (parameterize ((i-level (+ 1 (i-level))))
+                    (run* formals expr)))]
     ))
 
 (define-syntax (runi*/set stx)
     (syntax-case stx ()
         [(_ formals expr)
             #'(begin
-                (pretty-write (list 'run*/set (query formals expr)))
-                (run*/set formals expr))]
+                (pretty-write (list 'run*/set (list 'level (i-level)) (query formals expr)))
+                (parameterize ((i-level (+ 1 (i-level))))
+                    (run*/set formals expr)))]
     ))
 
 
