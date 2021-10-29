1fb93474a59c4e9835d2fdcf087e834b428d89e3
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-10-29 09:53:09 -0700

    kgx-synonym: implement: fill in missing code for kgx-synonym import from upstream file; remove redundant data fields

--- medikanren2/db/kgx-synonym.rkt
+++ medikanren2/db/kgx-synonym.rkt
@@ -4,9 +4,7 @@
 (require json "../base.rkt" (except-in racket/match ==))
 
 
-(define edge-keys '(subject object predicate id source_database))
-
-;(define in-edge (open-input-file "../data/KGX_NN_data_2021-03-11_edges.jsonl"))
+(define edge-keys '(subject object))
 
 (define kgid 'kgx-synonym)
 
@@ -14,23 +12,13 @@
   'path                 (if (cfg:config-ref 'migrated-to-new-db-versioning)
                           (path-for-database kgid 'synonym)
                           "kgx-synonym/010")
-  'source-file-path   (source-path-for-database kgid "KGX_NN_data_edges.jsonl")
   'attribute-names edge-keys
-  'attribute-types '(string string string string string)
+  'attribute-types '(string string)
   'indexes '((object subject))
-  #|'source-stream (value/syntax
-                  (let loop ()
-                    (lambda ()
-                      (define line (read-line (open-input-file "../data/KGX_NN_data_2021-03-11_edges.jsonl")))
-                      (cond ((eof-object? line) '())
-                            (else
-                             (define row-edge (string->jsexpr line))
-                             (cons (map (lambda (k) (hash-ref row-edge k)) edge-keys)
-                                   (loop)))))))|#
+  'source-stream   (value/syntax (source-stream-by-lines-from-strelpath (source-path-for-database kgid "KGX_NN_data_edges.jsonl")))
   'attribute-names    edge-keys
   'map                (value/syntax
-                        (lambda (row)
-                          (define js (list-ref row 0))
+                        (lambda (js)
                           (define jsexpr (string->jsexpr js))
                           (map (lambda (k) (hash-ref jsexpr k)) edge-keys)))
 
