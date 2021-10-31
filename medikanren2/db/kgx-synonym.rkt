36fc784ab2ddde015e886b3bfa410cfa50da05d7
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-10-30 19:41:03 -0700

    kgx-synonym: consume precooked jsonl instead of importing it

--- medikanren2/db/kgx-synonym.rkt
+++ medikanren2/db/kgx-synonym.rkt
@@ -15,7 +15,17 @@
   'attribute-names edge-keys
   'attribute-types '(string string)
   'indexes '((object subject))
-  'source-stream   (value/syntax (source-stream-by-lines-from-strelpath (source-path-for-database kgid "KGX_NN_data_edges.jsonl")))
+  ;*** cook like this ***
+  ;    (able to open stream but not able to test existence)
+  ;'source-stream   (value/syntax (source-stream-by-lines-from-strelpath (source-path-for-database kgid "KGX_NN_data_edges.jsonl")))
+
+  ;*** experiments ***
+  ;'source-file-path (in:file (source-path-for-database kgid "KGX_NN_data_edges.jsonl") 'format 'jsonl)
+  ;'source-file-path (source-stream-by-lines-from-strelpath (source-path-for-database kgid "KGX_NN_data_edges.jsonl"))
+
+  ;*** consume like this (points to nonexistent upstream file) ***
+  ;    (able to test nonexistence and move on, but not able to build)
+  'source-file-path (source-path-for-database kgid "KGX_NN_data_edges.jsonl")
   'attribute-names    edge-keys
   'map                (value/syntax
                         (lambda (js)
