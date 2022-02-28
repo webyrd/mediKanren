6960018711beb3a85023f77ab59f1ba82ec53c4a
William Byrd <webyrd@gmail.com>
2022-02-28 08:08:38 -0500

    updated mediKanren 1 script for indexing a KG by PUBMED identifiers

--- medikanren/util/data-import/build-pubmed-edges.rkt
+++ medikanren/util/data-import/build-pubmed-edges.rkt
@@ -1,9 +1,32 @@
 #lang racket/base
 (require
-  "../common.rkt"
-  "../repr.rkt"
+  "../../common.rkt"
+  "../../repr.rkt"
   (except-in racket/match ==)
   racket/stream)
+
+;; Script to index edges of a mediKanren 1 knowledge graph (KG) by PUBMED identifier.
+;;
+;;
+;; Command-line usage:
+;;
+;; racket build-pubmed-edges.rkt <path to mediKanren 1 data directory> <name of KG directory within the data directory>
+;;
+;;
+;; Example usage, on Will's laptop, for indexing rtx2_2021_02_04:
+;;
+;; racket build-pubmed-edges.rkt /Users/webyrd/github/mediKanren/medikanren/data rtx2_2021_02_04
+
+
+;; The script generates two files, 'edges.scm.offset' and 'edges.scm', in the directory of the KG being indexed.
+;;
+;; Once these files have been created, you can start Racket and load
+;; mediKanren 1 as usual.  Please make sure to require "common.rkt".
+;;
+;; To get a list of edges that contain the PUBMED id "10022988" (for example), you can use the expression:
+;;
+;; (run* (e) (pmid-edgeo "10022988" e)
+
 
 (define argv (current-command-line-arguments))
 (define argv-expected '#(DATA_DIR GRAPH_DIR))
@@ -30,25 +53,33 @@
 
 (define (process-pmids in-edges out-pubmed-edges out-offset-pubmed-edges)
   (define pmid=>eid* (hash))
-  (let loop ((eid&edge-stream (port->stream-offset&values in-edges)))
+  (let loop ((eid&edge-stream (port->stream-offset&values in-edges))
+             (edges-seen 0))
     (cond ((stream-empty? eid&edge-stream)
-           (printf "Found ~s pmids\n" (hash-count pmid=>eid*))
+           (printf "Found total ~s pmids\n" (hash-count pmid=>eid*))
            (for ((pmid (in-list (sort (hash-keys pmid=>eid*) string<?))))
                 (define eid* (hash-ref pmid=>eid* pmid))
                 (detail-write out-pubmed-edges out-offset-pubmed-edges
                               (cons pmid eid*)))
            (flush-output out-pubmed-edges)
            (flush-output out-offset-pubmed-edges))
-          (else (define eid&edge (stream-first eid&edge-stream))
-                (define eid (car eid&edge))
-                (define props (edge/props-props (cdr eid&edge)))
-                (for-each
-                  (lambda (pmid)
-                    (set! pmid=>eid*
-                      (hash-set pmid=>eid* pmid
-                                (cons eid (hash-ref pmid=>eid* pmid '())))))
-                  (pubmed-ids-from-edge-props props))
-                (loop (stream-rest eid&edge-stream))))))
+          (else
+           (define eid&edge (stream-first eid&edge-stream))
+           (define eid (car eid&edge))
+           (define props (edge/props-props (cdr eid&edge)))
+           (for-each
+             (lambda (pmid)
+               (set! pmid=>eid*
+                     (hash-set pmid=>eid* pmid
+                               (cons eid (hash-ref pmid=>eid* pmid '())))))
+             (pubmed-ids-from-edge-props props))
+
+           (when (= (modulo edges-seen 1000000) 0)
+             (printf "seen ~s edges\n" edges-seen)
+             (printf "found ~s pmids so far\n" (hash-count pmid=>eid*)))
+
+           (loop (stream-rest eid&edge-stream)
+                 (add1 edges-seen))))))
 
 (printf "\nMapping pmids to edges:\n")
 (time (call-with-input-file
