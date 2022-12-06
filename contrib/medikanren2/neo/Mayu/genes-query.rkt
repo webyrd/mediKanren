a46b109f4f50fe4480bffc30ca460b2d8e211cc4
William Byrd <webyrd@gmail.com>
2022-12-05 21:36:59 -0500

    added comments

--- contrib/medikanren2/neo/Mayu/genes-query.rkt
+++ contrib/medikanren2/neo/Mayu/genes-query.rkt
@@ -8,6 +8,12 @@
  racket/pretty
  racket/string)
 
+;; Problems encountered on 5 Dec 2022:
+;;
+;; * HGNC CURIES were not normalized to UMLS or NCBI or OMIM CURIES
+;;
+;; * We couldn't find any biolink:positively_regulates or
+;; biolink_negatively_regulates edges for the genes
 
 (define gene-curie-symbols '(HGNC:6884 HGNC:22082 HGNC:8032 HGNC:25088 HGNC:12009 HGNC:17997 HGNC:26509 HGNC:11254 HGNC:12855 HGNC:6192 HGNC:15968 HGNC:27230 HGNC:16493 HGNC:10542 HGNC:8851 HGNC:6323 HGNC:6482 HGNC:3811 HGNC:13450 HGNC:29433 HGNC:11581 HGNC:23537 HGNC:23166 HGNC:23159 HGNC:15573))
 
