95a599ccf2fa1037d8f98d5b2939a5a167407b3c
William Byrd <webyrd@gmail.com>
2023-05-15 15:03:46 -0400

    reduced number of answers to 250 for now, while we debug TRAPI 1.4 UI issues

--- medikanren2/neo/neo-server/neo-server.rkt
+++ medikanren2/neo/neo-server/neo-server.rkt
@@ -26,11 +26,11 @@
 
 (define DEFAULT_PORT 8384)
 
-(define NEO_SERVER_VERSION "1.7")
+(define NEO_SERVER_VERSION "1.8")
 
 ;; Maximum number of results to be returned from *each individual* KP,
 ;; or from mediKanren itself.
-(define MAX_RESULTS_FROM_COMPONENT 1000)
+(define MAX_RESULTS_FROM_COMPONENT 250)
 
 ;; Maximum number of results to score and then sort.
 (define MAX_RESULTS_TO_SCORE_AND_SORT 100000)
