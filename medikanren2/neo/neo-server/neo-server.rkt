590dacc7374144f6c391df9250733fb18a5efd2c
kaiwenho <kaiwenhe@uab.edu>
2023-06-01 13:41:35 -0500

    add all subjects and objects from knowledge_graph.edges to knowledge_graph.nodes besides the nodes from two-hop results

--- medikanren2/neo/neo-server/neo-server.rkt
+++ medikanren2/neo/neo-server/neo-server.rkt
@@ -26,7 +26,7 @@
 
 (define DEFAULT_PORT 8384)
 
-(define NEO_SERVER_VERSION "1.13")
+(define NEO_SERVER_VERSION "1.15")
 
 ;; Maximum number of results to be returned from *each individual* KP,
 ;; or from mediKanren itself.
@@ -1288,16 +1288,20 @@
                              'name name)))))
       
       (define (add-edge! props n)             
-        (let ((id (string-append "medik:edge#" (number->string n))))
+        (let ((id (string-append "medik:edge#" (number->string n)))
+              (object (get-assoc "object" props))
+              (subject (get-assoc "subject" props)))
+          (add-node! object)
+          (add-node! subject)
           (hash-set! edges (string->symbol id)
                      (hash 'attributes
                            (or
                             (and (get-assoc "json_attributes" props)
                                  (string->jsexpr (get-assoc "json_attributes" props)))
                             (data-attributes props))
-                           'object (get-assoc "object" props)
+                           'object object
                            'predicate (get-assoc "predicate" props)
-                           'subject (get-assoc "subject" props)
+                           'subject subject
                            'sources (list (get-source props) unsecret-source)))
           id))
 
@@ -1359,9 +1363,9 @@
               (if (and (edge-has-source? props_xy)
                        (edge-has-source? props_yz))
                   (begin 
-                    (add-node! curie_x)
-                    (add-node! curie_y)
-                    (add-node! curie_z)
+                    ;(add-node! curie_x)
+                    ;(add-node! curie_y)
+                    ;(add-node! curie_z)
                     (let* ((edge_xy (add-edge! props_xy en))
                            (edge_yz (add-edge! props_yz (+ en 1)))
                            (auxiliary_id (add-auxiliary! (list edge_xy edge_yz) an))
@@ -1417,8 +1421,8 @@
                 ,props_xy)
               (if (edge-has-source? props_xy)
                   (begin
-                    (add-node! curie_x)
-                    (add-node! curie_y)
+                    ;(add-node! curie_x)
+                    ;(add-node! curie_y)
                     (let ((edge_xy (add-edge! props_xy en)))
                       (add-unmerged-result!
                        (cond
