cd02cd4ee5f80372e8ad7e02b8cf72d63d8f9842
William Byrd <webyrd@gmail.com>
2023-06-01 13:45:49 -0400

    added examples

--- contrib/medikanren2/neo/Navya/simple-query.rkt
+++ contrib/medikanren2/neo/Navya/simple-query.rkt
@@ -21,3 +21,30 @@
     (get-descendent-curies*-in-db
      (curies->synonyms-in-db
       (list "DOID:9351"))))))
+
+(define diabetes-treatments
+  (query:X->Known
+   #f
+   (set->list
+    (get-non-deprecated-mixed-ins-and-descendent-predicates*-in-db
+     '("biolink:treats")))
+   (set->list
+    (get-descendent-curies*-in-db
+     (curies->synonyms-in-db
+      (list "DOID:9351"))))))
+
+(curies->synonyms-in-db (list "PUBCHEM.COMPOUND:5291"))
+;; =>
+'("PUBCHEM.COMPOUND:5291"
+  "PUBCHEM.COMPOUND:123596"
+  "DRUGBANK:DB00619")
+
+(define imatinib-regulates
+  (query:Known->X
+   (set->list
+    (get-descendent-curies*-in-db
+     (curies->synonyms-in-db (list "PUBCHEM.COMPOUND:5291"))))
+   (set->list
+    (get-non-deprecated-mixed-ins-and-descendent-predicates*-in-db
+     '("biolink:regulates")))
+   #f))
