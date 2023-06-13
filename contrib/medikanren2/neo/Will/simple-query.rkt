3a3c400bb4e3051a3cc96ac95d4bbc337d24c1f8
William Byrd <webyrd@gmail.com>
2023-06-12 23:56:38 -0400

    added EGFR queries

--- contrib/medikanren2/neo/Will/simple-query.rkt
+++ contrib/medikanren2/neo/Will/simple-query.rkt
@@ -11,6 +11,32 @@
  racket/pretty
  racket/string)
 
+(define regulates-EGFR
+  (query:X->Known
+   #f
+   (set->list
+    (get-non-deprecated-mixed-ins-and-descendent-predicates*-in-db
+     '("biolink:regulates")))
+   (set->list
+    (get-descendent-curies*-in-db
+     (curies->synonyms-in-db (list "HGNC:3236"))))))
+
+(define downregulates-EGFR
+  (filter
+   (lambda (e)
+     (match e
+       [`(,subj ,pred ,obj . ,props)
+        (and (member '("predicate" "biolink:regulates") props)
+             (member '("object_direction_qualifier" "downregulated") props))]))
+   (query:X->Known
+    #f
+    (set->list
+     (get-non-deprecated-mixed-ins-and-descendent-predicates*-in-db
+      '("biolink:affects")))
+    (set->list
+     (get-descendent-curies*-in-db
+      (curies->synonyms-in-db (list "HGNC:3236")))))))
+
 (define diabetes-causes
   (query:X->Known
    #f
